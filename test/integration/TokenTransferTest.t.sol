4aa7a4774f899d17de5592ce0e267bcdcc965208
Jacob Evans <jacob@dekz.net>
2023-06-02 14:37:30 +1000

    wtf: clear token transfer test

--- test/integration/TokenTransferTest.t.sol
+++ test/integration/TokenTransferTest.t.sol
@@ -1,37 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {ERC20} from "solmate/src/tokens/ERC20.sol";
-
-import {SafeTransferLib} from "../../src/utils/SafeTransferLib.sol";
-
 import {BasePairTest} from "./BasePairTest.t.sol";
 
 abstract contract TokenTransferTest is BasePairTest {
-    using SafeTransferLib for ERC20;
 
     function setUp() public virtual override {
         super.setUp();
-    }
-
-    function testToken_transfer_entire() public {
-        vm.startPrank(FROM);
-        deal(address(fromToken()), FROM, amount());
-        fromToken().safeTransfer(BURN_ADDRESS, amount());
-
-        deal(address(toToken()), FROM, amount());
-        toToken().safeTransfer(BURN_ADDRESS, amount());
-    }
-
-    function testToken_transferFrom_entire() public {
-        address spender = address(this);
-
-        deal(address(fromToken()), FROM, amount());
-        safeApproveIfBelow(fromToken(), FROM, spender, amount());
-        fromToken().safeTransferFrom(FROM, BURN_ADDRESS, amount());
-
-        deal(address(toToken()), FROM, amount());
-        safeApproveIfBelow(toToken(), FROM, spender, amount());
-        toToken().safeTransferFrom(FROM, BURN_ADDRESS, amount());
     }
 }
