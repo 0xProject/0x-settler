1b2dc3c0fe0f6db697c38a8fe38b402c93902f25
abls <112491550+abls@users.noreply.github.com>
2023-08-27 11:50:14 -0700

    emit events on weth wrap/unwrap

--- src/core/WethWrap.sol
+++ src/core/WethWrap.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.21;
 import {WETH} from "solmate/src/tokens/WETH.sol";
 
 abstract contract WethWrap {
-    /// @dev WETH address
+    event WethDeposit(uint256 wad);
+    event WethWithdrawal(uint256 wad);
+
     WETH private immutable _weth;
 
     constructor(address payable weth) {
@@ -13,9 +15,11 @@ abstract contract WethWrap {
 
     function depositWeth(uint256 amount) internal {
         _weth.deposit{value: amount}();
+        emit WethDeposit(amount);
     }
 
     function withdrawWeth(uint256 amount) internal {
         _weth.withdraw(amount);
+        emit WethWithdrawal(amount);
     }
 }
