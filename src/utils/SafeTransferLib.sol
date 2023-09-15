4de0fbe152d75ad0111e9285d5c296383ef4bf65
Duncan Townsend <git@duncancmt.com>
2023-09-15 11:42:09 -0400

    WIP: modify SafeTransferLib

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -58,8 +58,6 @@ library SafeTransferLib {
     }
 
     function safeTransfer(ERC20 token, address to, uint256 amount) internal {
-        bool success;
-
         assembly ("memory-safe") {
             // Get a pointer to some free memory.
             let freeMemoryPointer := mload(0x40)
@@ -69,20 +67,19 @@ library SafeTransferLib {
             mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
             mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument. Masking not required as it's a full 32 byte type.
 
-            success :=
-                and(
-                    // Set success to whether the call reverted, if not we check it either
-                    // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
-                    // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
-                    // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                    // Counterintuitively, this call must be positioned second to the or() call in the
-                    // surrounding and() call or else returndatasize() will be zero during the computation.
-                    call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
-                )
+            // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
+            // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+            if iszero(call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)) {
+                returndatacopy(freeMemoryPointer, 0, returndatasize())
+                revert(freeMemoryPointer, returndatasize())
+            }
+            // We check that the call either returned exactly 1 (can't just be non-zero data), or had no
+            // return data.
+            if iszero(or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize()))) {
+                mstore(0, _TRANSFER_FAILED_SELECTOR)
+                revert(0x1c, 0x04)
+            }
         }
-
-        require(success, "TRANSFER_FAILED");
     }
 
     function safeApprove(ERC20 token, address to, uint256 amount) internal {
