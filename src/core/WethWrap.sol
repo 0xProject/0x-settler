672c3f1bb2636e4995bf094e5307b498002a1854
Duncan Townsend <git@duncancmt.com>
2023-09-15 10:41:00 -0400

    Remove events

--- src/core/WethWrap.sol
+++ src/core/WethWrap.sol
@@ -7,9 +7,6 @@ import {FullMath} from "../utils/FullMath.sol";
 abstract contract WethWrap {
     using FullMath for uint256;
 
-    event WethDeposit(uint256 wad);
-    event WethWithdrawal(uint256 wad);
-
     WETH private immutable _weth;
 
     constructor(address payable weth) {
@@ -19,13 +16,11 @@ abstract contract WethWrap {
     function depositWeth(uint256 bips) internal {
         uint256 amount = address(this).balance.mulDiv(bips, 10_000);
         _weth.deposit{value: amount}();
-        emit WethDeposit(amount);
     }
 
     function withdrawWeth(uint256 bips) internal {
         uint256 amount = _weth.balanceOf(address(this)).mulDiv(bips, 10_000);
         _weth.withdraw(amount);
-        emit WethWithdrawal(amount);
     }
 
     receive() external payable {}
