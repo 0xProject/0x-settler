652df58a200b2446d1d841ad93de47e5acd77270
Duncan Townsend <git@duncancmt.com>
2024-02-06 13:56:59 -0500

    Golf

--- src/utils/Create3.sol
+++ src/utils/Create3.sol
@@ -8,7 +8,7 @@ import {Panic} from "./Panic.sol";
         // constructor
         | Address | Bytecode | Mnemonic | Stack                             | Memory
         ----------------------------------------------------------------------------
-/-----< | 00 | 6022 | push1 0x22   | [runtimeLen]                           | {}
+/-----< | 00 | 6021 | push1 0x22   | [runtimeLen]                           | {}
 |       | 02 | 80   | dup1         | [runtimeLen runtimeLen]                | {}
 |   /-< | 03 | 6009 | push1 0x09   | [runtimeStart runtimeLen runtimeLen]   | {}
 |   |   | 05 | 5f   | push0        | [0 runtimeStart runtimeLen runtimeLen] | {}
@@ -22,7 +22,7 @@ import {Panic} from "./Panic.sol";
 |       ----------------------------------------------------------------------------
 |       | 00 | 5f   | push0        | [0]                                    | {}
 |       | 01 | 54   | sload        | [initialized]                          | {}
-| /---< | 02 | 601f | push1 0x1f   | [target initialized}                   | {}
+| /---< | 02 | 601e | push1 0x1f   | [target initialized}                   | {}
 | |     | 04 | 57   | jumpi        | []                                     | {}
 | |     | 05 | 6001 | push1 0x01   | [1]                                    | {}
 | |     | 07 | 5f   | push0        | [0 1]                                  | {}
@@ -35,31 +35,29 @@ import {Panic} from "./Panic.sol";
 | |     | 0e | 5f   | push0        | [0 cds]                                | {initCode}
 | |     | 0f | 34   | callvalue    | [msg.value 0 cds]                      | {initCode}
 | |     | 10 | f0   | create       | [deployed]                             | {initCode}
-| |     | 11 | 80   | dup1         | [deployed deployed]                    | {initCode}
-| | /-< | 12 | 6018 | push1 0x18   | [target deployed deployed]             | {initCode}
-| | |   | 14 | 57   | jumpi        | [deployed]                             | {initCode}
-| | |   | 15 | 5f   | push0        | [0 deployed]                           | {initCode}
+| |     | 11   5f   | push0        | [0 deployed]                           | {initCode}
+| |     | 12 | 81   | dup2         | [deployed 0 deployed]                  | {initCode}
+| | /-< | 13 | 6018 | push1 0x18   | [target deployed 0 deployed]           | {initCode}
+| | |   | 15 | 57   | jumpi        | [0 deployed]                           | {initCode}
 | | |   | 16 | 5f   | push0        | [0 0 deployed]                         | {initCode}
 | | |   | 17 | fd   | revert       | X                                      | X
-| | \-> | 18 | 5b   | jumpdest     | [deployed]                             | {initCode}
-| |     | 19 | 5f   | push0        | [0 deployed]                           | {initCode}
-| |     | 1a | 52   | mstore       | []                                     | {deployed ...}
-| |     | 1b | 6020 | push1 0x20   | [32]                                   | {deployed ...}
-| |     | 1d | 5f   | push0        | [0 32]                                 | {deployed ...}
-| |     | 1e | f3   | return       | X                                      | X
-| \---> | 1f | 5b   | jumpdest     | []                                     | {}
-|       | 20 | 32   | origin       | [tx.origin]                            | {}
-|       | 21 | ff   | selfdestruct | X                                      | {}
-\-----> | 22 |
-
+| | \-> | 18 | 5b   | jumpdest     | [0 deployed]                           | {initCode}
+| |     | 19 | 52   | mstore       | []                                     | {deployed ...}
+| |     | 1a | 6020 | push1 0x20   | [32]                                   | {deployed ...}
+| |     | 1c | 5f   | push0        | [0 32]                                 | {deployed ...}
+| |     | 1d | f3   | return       | X                                      | X
+| \---> | 1e | 5b   | jumpdest     | []                                     | {}
+|       | 1f | 32   | origin       | [tx.origin]                            | {}
+|       | 20 | ff   | selfdestruct | X                                      | {}
+\-----> | 21 |
 */
 
 library Create3 {
-    uint256 private constant _SHIM0 = 0x60228060095f395ff35f54601f5760015f55365f5f37365f34f0806018575f5f;
-    uint88 private constant _SHIM1 = 0xfd5b5f5260205ff35b32ff;
-    uint8 private constant _SHIM1_LENGTH = 0x0b;
-    uint8 private constant _SHIM_LENGTH = 0x2b;
-    bytes32 private constant _SHIM_INITHASH = 0x8caf384a020ba9b2d885eaa7c5b13f74d8707a1cf4b78bf810826ae67d234532;
+    uint256 private constant _SHIM0 = 0x60218060095f395ff35f54601e5760015f55365f5f37365f34f05f816018575f;
+    uint80 private constant _SHIM1 = 0xfd5b5260205ff35b32ff;
+    uint8 private constant _SHIM1_LENGTH = 0x0a;
+    uint8 private constant _SHIM_LENGTH = 0x2a;
+    bytes32 private constant _SHIM_INITHASH = 0x27fc81c6e9350a4023c28ecd8d6d2e02ee484971c16865596228e8241edb4290;
 
     function createFromCalldata(bytes32 salt, bytes calldata initCode, uint256 value)
         internal
