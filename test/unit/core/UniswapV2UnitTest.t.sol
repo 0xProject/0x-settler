bb7b1ea0bea6f080a74e18b59984fe448232ad7a
Duncan Townsend <git@duncancmt.com>
2024-02-09 19:51:56 -0500

    Use `abi.encodeCall`

--- test/unit/core/UniswapV2UnitTest.t.sol
+++ test/unit/core/UniswapV2UnitTest.t.sol
@@ -42,16 +42,14 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 amount = 99999;
         uint256 minBuyAmount = 9087;
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         _mockExpectCall(
-            POOL,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(9087), 0, RECIPIENT, new bytes(0))),
-            new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(9087), 0, RECIPIENT, new bytes(0)), new bytes(0)
         );
 
         uni.sell(RECIPIENT, TOKEN0, POOL, TOKEN0 < TOKEN1 ? 1 : 0, bips, minBuyAmount);
@@ -62,16 +60,14 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 amount = 99999;
         uint256 minBuyAmount = 1e18;
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         _mockExpectCall(
-            POOL,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(9087), 0, RECIPIENT, new bytes(0))),
-            new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(9087), 0, RECIPIENT, new bytes(0)), new bytes(0)
         );
 
         vm.expectRevert();
@@ -83,16 +79,14 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 amount = 99999;
         uint256 minBuyAmount = 1;
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount / 2));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount / 2), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount / 2));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount / 2)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         _mockExpectCall(
-            POOL,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(8328), 0, RECIPIENT, new bytes(0))),
-            new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(8328), 0, RECIPIENT, new bytes(0)), new bytes(0)
         );
 
         uni.sell(RECIPIENT, TOKEN0, POOL, TOKEN0 < TOKEN1 ? 1 : 0, bips, minBuyAmount);
@@ -103,16 +97,14 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 amount = 99999;
         uint256 minBuyAmount = 9521;
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount * 2));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount * 2), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount * 2));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount * 2)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         _mockExpectCall(
-            POOL,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(9521), 0, RECIPIENT, new bytes(0))),
-            new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(9521), 0, RECIPIENT, new bytes(0)), new bytes(0)
         );
 
         uni.sell(RECIPIENT, TOKEN0, POOL, TOKEN0 < TOKEN1 ? 1 : 0, bips, minBuyAmount);
@@ -124,18 +116,16 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 minBuyAmount = 1;
 
         // We emulate a token which has a 50% fee when transferring to the Uniswap pool
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, POOL), abi.encode(amount / 2));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (POOL)), abi.encode(amount / 2));
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         _mockExpectCall(
-            POOL,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(7994), 0, RECIPIENT, new bytes(0))),
-            new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(7994), 0, RECIPIENT, new bytes(0)), new bytes(0)
         );
         // the pool is responsible for transferring to receipient, since the pool is a dummy, this transfer is not mocked
 
@@ -147,23 +137,23 @@ contract UniswapV2UnitTest is Utils, Test {
         uint256 amount = 99999;
         uint256 minBuyAmount = 9521;
 
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.balanceOf.selector, address(uni)), abi.encode(amount * 2));
-        _mockExpectCall(TOKEN0, abi.encodeWithSelector(IERC20.transfer.selector, POOL, amount * 2), new bytes(0));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.balanceOf, (address(uni))), abi.encode(amount * 2));
+        _mockExpectCall(TOKEN0, abi.encodeCall(IERC20.transfer, (POOL, amount * 2)), new bytes(0));
 
         // UniswapV2Pool.getReserves
-        _mockExpectCall(POOL, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         // UniswapV2Pool.swap
         //   POOL specifies POOL2 as recipient
         _mockExpectCall(
-            POOL, abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(9521), 0, POOL2, new bytes(0))), new bytes(0)
+            POOL, abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(9521), 0, POOL2, new bytes(0)), new bytes(0)
         );
-        _mockExpectCall(POOL2, abi.encodePacked(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
+        _mockExpectCall(POOL2, abi.encodeWithSelector(bytes4(0x0902f1ac)), abi.encode(uint256(9999), uint256(9999)));
         _mockExpectCall(TOKEN1, abi.encodeCall(IERC20.balanceOf, (POOL2)), abi.encode(amount * 2 + 9999));
         // UniswapV2Pool.swap
         //   POOL2 specifies RECIPIENT as recipient
         _mockExpectCall(
             POOL2,
-            abi.encodePacked(bytes4(0x022c0d9f), abi.encode(uint256(0), uint256(9521), RECIPIENT, new bytes(0))),
+            abi.encodeWithSelector(bytes4(0x022c0d9f), uint256(0), uint256(9521), RECIPIENT, new bytes(0)),
             new bytes(0)
         );
 
