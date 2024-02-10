33519fad28c9006b69c180eb5bc369db1e96411e
Duncan Townsend <git@duncancmt.com>
2024-02-09 23:00:51 -0500

    Use MultiCall base class for Deployer

--- src/deployer/Deployer.sol
+++ src/deployer/Deployer.sol
@@ -8,44 +8,7 @@ import {Panic} from "../utils/Panic.sol";
 import {Create3} from "../utils/Create3.sol";
 import {IPFS} from "../utils/IPFS.sol";
 import {ItoA} from "../utils/ItoA.sol";
-import {Revert} from "../utils/Revert.sol";
-
-library UnsafeArray {
-    function unsafeGet(bytes[] calldata datas, uint256 i) internal pure returns (bytes calldata data) {
-        assembly ("memory-safe") {
-            // helper functions
-            function overflow() {
-                mstore(0x00, 0x4e487b71) // keccak256("Panic(uint256)")[:4]
-                mstore(0x20, 0x11) // 0x11 -> arithmetic under-/over- flow
-                revert(0x1c, 0x24)
-            }
-            function bad_calldata() {
-                revert(0x00, 0x00) // empty reason for malformed calldata
-            }
-
-            // initially, we set `data.offset` to the pointer to the length. this is 32 bytes before the actual start of data
-            data.offset :=
-                add(
-                    datas.offset,
-                    calldataload(
-                        add(shl(5, i), datas.offset) // can't overflow; we assume `i` is in-bounds
-                    )
-                )
-            // because the offset to `data` stored in `datas` is arbitrary, we have to check it
-            if lt(data.offset, add(shl(5, datas.length), datas.offset)) { overflow() }
-            if iszero(lt(data.offset, calldatasize())) { bad_calldata() }
-            // now we load `data.length` and set `data.offset` to the start of datas
-            data.length := calldataload(data.offset)
-            data.offset := add(data.offset, 0x20) // can't overflow; calldata can't be that long
-            {
-                // check that the end of `data` is in-bounds
-                let end := add(data.offset, data.length)
-                if lt(end, data.offset) { overflow() }
-                if gt(end, calldatasize()) { bad_calldata() }
-            }
-        }
-    }
-}
+import {MultiCall} from "../utils/MultiCall.sol";
 
 interface IERC721View is IERC165 {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -62,9 +25,7 @@ interface IERC721ViewMetadata is IERC721View {
     function tokenURI(uint256) external view returns (string memory);
 }
 
-contract Deployer is ERC1967UUPSUpgradeable, Context, ERC1967TwoStepOwnable, IERC721ViewMetadata {
-    using UnsafeArray for bytes[];
-
+contract Deployer is ERC1967UUPSUpgradeable, Context, ERC1967TwoStepOwnable, IERC721ViewMetadata, MultiCall {
     struct DoublyLinkedList {
         uint64 prev;
         uint64 next;
@@ -263,26 +224,6 @@ contract Deployer is ERC1967UUPSUpgradeable, Context, ERC1967TwoStepOwnable, IER
         }
         emit RemovedAll(feature);
         return true;
-    }
-
-    // in spite of the fact that `deploy` is payable, `multicall` cannot be
-    // payable for security. therefore, there are some use cases where it is
-    // necessary to make multiple calls to this contract.
-    function multicall(bytes[] calldata datas) public {
-        uint256 freeMemPtr;
-        address target = _implementation;
-        assembly ("memory-safe") {
-            freeMemPtr := mload(0x40)
-        }
-        unchecked {
-            for (uint256 i; i < datas.length; i++) {
-                (bool success, bytes memory reason) = target.delegatecall(datas.unsafeGet(i));
-                Revert.maybeRevert(success, reason);
-                assembly ("memory-safe") {
-                    mstore(0x40, freeMemPtr)
-                }
-            }
-        }
     }
 
     string public constant override name = "0xV5";
