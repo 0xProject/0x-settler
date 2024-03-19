f217e2fe0f9ade8a8bc412a21b5a841511310a1c
Duncan Townsend <git@duncancmt.com>
2024-03-19 11:49:05 -0400

    Make SafeModule a little more user friendly

--- src/deployer/SafeModule.sol
+++ src/deployer/SafeModule.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {IDeployer} from "./IDeployer.sol";
 import {Feature} from "./Feature.sol";
 import {Nonce} from "./Nonce.sol";
+import {Revert} from "../utils/Revert.sol";
 
 interface ISafeMinimal {
     enum Operation {
@@ -19,24 +20,44 @@ interface ISafeMinimal {
 }
 
 contract ZeroExSettlerDeployerSafeModule {
+    using Revert for bool;
+
     ISafeMinimal public immutable safe;
     IDeployer public constant deployer = IDeployer(0x00000000000004533Fe15556B1E086BB1A72cEae);
-
-    bytes4 internal constant removeByNonceSelector = bytes4(keccak256("remove(uint128,uint32)"));
-    bytes4 internal constant removeByInstanceSelector = bytes4(keccak256("remove(address)"));
-    bytes4 internal constant removeAllSelector = IDeployer.removeAll.selector;
 
     constructor(address _safe) {
         safe = ISafeMinimal(_safe);
     }
 
-    function exec(bytes calldata data) external returns (bool success, bytes memory returnData) {
+    modifier onlyOwner() {
         require(safe.isOwner(msg.sender));
-        bytes4 selector = bytes4(data);
-        require(
-            selector == removeByNonceSelector || selector == removeByInstanceSelector || selector == removeAllSelector
+        _;
+    }
+
+    function remove(Feature feature, Nonce nonce) external onlyOwner returns (bool) {
+        (bool success, bytes memory returnData) = safe.execTransactionFromModuleReturnData(
+            address(deployer),
+            0,
+            abi.encodeWithSignature("remove(uint128,uint32)", feature, nonce),
+            ISafeMinimal.Operation.Call
         );
-        (success, returnData) =
-            safe.execTransactionFromModuleReturnData(address(deployer), 0, data, ISafeMinimal.Operation.Call);
+        success.maybeRevert(returnData);
+        return abi.decode(returnData, (bool));
+    }
+
+    function remove(address instance) external onlyOwner returns (bool) {
+        (bool success, bytes memory returnData) = safe.execTransactionFromModuleReturnData(
+            address(deployer), 0, abi.encodeWithSignature("remove(address)", (instance)), ISafeMinimal.Operation.Call
+        );
+        success.maybeRevert(returnData);
+        return abi.decode(returnData, (bool));
+    }
+
+    function removeAll(Feature feature) external onlyOwner returns (bool) {
+        (bool success, bytes memory returnData) = safe.execTransactionFromModuleReturnData(
+            address(deployer), 0, abi.encodeCall(deployer.removeAll, (feature)), ISafeMinimal.Operation.Call
+        );
+        success.maybeRevert(returnData);
+        return abi.decode(returnData, (bool));
     }
 }
