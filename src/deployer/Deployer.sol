e8df25b83fb6beb0089df29613ca87569e131a27
Duncan Townsend <git@duncancmt.com>
2024-03-21 12:46:29 -0400

    Remove footgun

--- src/deployer/Deployer.sol
+++ src/deployer/Deployer.sol
@@ -140,6 +140,7 @@ contract Deployer is IDeployer, ERC1967UUPSUpgradeable, Context, ERC1967TwoStepO
 
     function initialize(address initialOwner) external {
         require(address(this) == 0x00000000000004533Fe15556B1E086BB1A72cEae || block.chainid == 31337);
+        require(initialOwner.code.length != 0);
         _setPendingOwner(initialOwner);
         super._initialize();
     }
