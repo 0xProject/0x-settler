db044927ba97fe084916717f7a3c1333806622a5
Duncan Townsend <git@duncancmt.com>
2024-04-01 04:39:07 -0400

    Ignore `secrets.json`; boilerplate for deployment Bash script

---
+++ sh/deploy_new_chain.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+## POSIX Bash implementation of realpath
+## Copied and modified from https://github.com/mkropat/sh-realpath and https://github.com/AsymLabs/realpath-lib/
+## Copyright (c) 2014 Michael Kropat - MIT License
+## Copyright (c) 2013 Asymmetry Laboratories - MIT License
+
+realpath() {
+    _resolve_symlinks "$(_canonicalize "$1")"
+}
+
+_directory() {
+    local out slsh
+    slsh=/
+    out="$1"
+    out="${out//$slsh$slsh/$slsh}"
+    if [ "$out" = / ]; then
+        echo /
+        return
+    fi
+    out="${out%/}"
+    case "$out" in
+        */*)
+            out="${out%/*}"
+        ;;
+        *)
+            out=.
+        ;;
+    esac
+    if [ "$out" ]; then
+        printf '%s\n' "$out"
+    else
+        echo /
+    fi
+}
+
+_file() {
+    local out slsh
+    slsh=/
+    out="$1"
+    out="${out//$slsh$slsh/$slsh}"
+    if [ "$out" = / ]; then
+        echo /
+        return
+    fi
+    out="${out%/}"
+    out="${out##*/}"
+    printf '%s\n' "$out"
+}
+
+_resolve_symlinks() {
+    local path pattern context
+    while [ -L "$1" ]; do
+        context="$(_directory "$1")"
+        path="$(POSIXLY_CORRECT=y ls -ld -- "$1" 2>/dev/null)"
+        pattern='*'"$(_escape "$1")"' -> '
+        path="${path#$pattern}"
+        set -- "$(_canonicalize "$(_prepend_context "$context" "$path")")" "$@"
+        _assert_no_path_cycles "$@" || return 1
+    done
+    printf '%s\n' "$1"
+}
+
+_escape() {
+    local out
+    out=''
+    local -i i
+    for ((i=0; i < ${#1}; i+=1)); do
+        out+='\'"${1:$i:1}"
+    done
+    printf '%s\n' "$out"
+}
+
+_prepend_context() {
+    if [ "$1" = . ]; then
+        printf '%s\n' "$2"
+    else
+        case "$2" in
+            /* ) printf '%s\n' "$2" ;;
+             * ) printf '%s\n' "$1/$2" ;;
+        esac
+    fi
+}
+
+_assert_no_path_cycles() {
+    local target path
+
+    if [ $# -gt 16 ]; then
+        return 1
+    fi
+
+    target="$1"
+    shift
+
+    for path in "$@"; do
+        if [ "$path" = "$target" ]; then
+            return 1
+        fi
+    done
+}
+
+_canonicalize() {
+    local d f
+    if [ -d "$1" ]; then
+        (CDPATH= cd -P "$1" 2>/dev/null && pwd -P)
+    else
+        d="$(_directory "$1")"
+        f="$(_file "$1")"
+        (CDPATH= cd -P "$d" 2>/dev/null && printf '%s/%s\n' "$(pwd -P)" "$f")
+    fi
+}
+
+## end POSIX Bash implementation of realpath
+
+set -Eeufo pipefail -o posix
+
+declare SCRIPT_DIR
+SCRIPT_DIR="$(_directory "$(_directory "$(realpath "${BASH_SOURCE[0]}")")")"
+declare -r SCRIPT_DIR
+cd "$SCRIPT_DIR"
+
+if ! hash jq &>/dev/null ; then
+    echo "jq isn't installed" >&2
+    exit 1
+fi
+
+if ! hash sha256sum &>/dev/null ; then
+    echo "sha256sum isn't installed" >&2
+    exit 1
+fi
+
+if [ ! -f secrets.json ] ; then
+    echo "secrets.json is missing" >&2
+    exit 1
+fi
+
+if [[ $(stat -L -c "%a" secrets.json) != "600" ]] ; then
+    echo "secrets.json permissions too lax" >&2
+    echo "run: chmod 600 secrets.json" >&2
+    exit 1
+fi
+
+if ! sha256sum -c <<<"24290900be9575d1fb6349098b1c11615a2eac8091bc486bec6cf67239b7846a  secrets.json" >/dev/null ; then
+    echo "Secrets are wrong" >&2
+    exit 1
+fi
+
+echo "begin script"
