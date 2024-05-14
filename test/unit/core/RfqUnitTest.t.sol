d0c7251e0d5acf58b26897513bdd7ef27f55e5fb
Duncan Townsend <git@duncancmt.com>
2024-05-14 14:18:03 -0400

    Fix unit test

--- test/unit/core/RfqUnitTest.t.sol
+++ test/unit/core/RfqUnitTest.t.sol
@@ -118,7 +118,7 @@ contract RfqUnitTest is Utils, Test {
     RfqOrderSettlementDummy rfq;
     RfqOrderSettlementMetaTxnDummy rfqMeta;
     address PERMIT2 = _etchNamedRejectionDummy("PERMIT2", 0x000000000022D473030F116dDEE9F6B43aC78BA3);
-    address ALLOWANCE_HOLDER = _etchNamedRejectionDummy("ALLOWANCE_HOLDER", 0x0000000000001fF3684f28c67538d4D072C22734);
+    address ALLOWANCE_HOLDER = _etchNamedRejectionDummy("ALLOWANCE_HOLDER", 0x0000000000005E88410CcDFaDe4a5EfaE4b49562);
 
     address TOKEN0 = _createNamedRejectionDummy("TOKEN0");
     address TOKEN1 = _createNamedRejectionDummy("TOKEN1");
