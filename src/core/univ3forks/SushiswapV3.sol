38ee0a43e5fe840226e4d2154ee53238de7d85ac
Duncan Townsend <git@duncancmt.com>
2024-05-16 12:11:23 -0400

    Compilation error

--- src/core/univ3forks/SushiswapV3.sol
+++ src/core/univ3forks/SushiswapV3.sol
@@ -2,4 +2,4 @@
 pragma solidity ^0.8.25;
 
 address constant sushiswapV3Factory = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
-bytes32 constant sushiswapV3InitHash = e34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+bytes32 constant sushiswapV3InitHash = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
