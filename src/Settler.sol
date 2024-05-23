fe1b30d2381a4d102a7a672b54e848f11395e442
Duncan Townsend <git@duncancmt.com>
2024-05-23 17:31:58 -0400

    Add a deploy-time check that Settler owns the Deployer NFT

--- src/Settler.sol
+++ src/Settler.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 
 import {IERC20, IERC20Meta} from "./IERC20.sol";
+import {IERC721Owner} from "./IERC721Owner.sol";
 import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
 
 import {Permit2PaymentBase} from "./core/Permit2Payment.sol";
@@ -18,6 +19,13 @@ import {ActionInvalid} from "./core/SettlerErrors.sol";
 abstract contract Settler is AllowanceHolderContext, SettlerBase {
     using UnsafeMath for uint256;
     using CalldataDecoder for bytes[];
+
+    constructor() {
+        assert(
+            block.chainid == 31337
+                || IERC721Owner(0x00000000000004533Fe15556B1E086BB1A72cEae).ownerOf(2) == address(this)
+        );
+    }
 
     function _hasMetaTxn() internal pure override returns (bool) {
         return false;
