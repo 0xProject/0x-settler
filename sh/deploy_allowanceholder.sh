a65d92aa84d410c383bbdadf7724a4e7e3e65fa5
Duncan Townsend <git@duncancmt.com>
2024-07-15 18:43:15 -0400

    Streamline `AllowanceHolder` deployment

--- sh/deploy_allowanceholder.sh
+++ sh/deploy_allowanceholder.sh
@@ -142,7 +142,7 @@ declare -r -i gas_price
 export FOUNDRY_OPTIMIZER_RUNS=1000000
 
 forge clean
-forge create --no-cache --private-key "$(get_secret allowanceHolderOld key)" --chain "$(get_config chainId)" --rpc-url "$rpc_url" --gas-price "$gas_price" --gas-limit 4000000 --etherscan-api-key "$(get_api_secret etherscanKey)" --verifier-url "$(get_config etherscanApi)" --verify $(get_config extraFlags) src/allowanceholder/AllowanceHolderOld.sol:AllowanceHolder
+forge create --private-key "$(get_secret allowanceHolderOld key)" --chain "$(get_config chainId)" --rpc-url "$rpc_url" --gas-price "$gas_price" --gas-limit 4000000 --etherscan-api-key "$(get_api_secret etherscanKey)" --verifier-url "$(get_config etherscanApi)" --verify $(get_config extraFlags) src/allowanceholder/AllowanceHolderOld.sol:AllowanceHolder
 forge verify-contract --watch --chain "$(get_config chainId)" --verifier sourcify --optimizer-runs 1000000 --constructor-args 0x "$(get_secret allowanceHolderOld address)" src/allowanceholder/AllowanceHolderOld.sol:AllowanceHolder
 
 echo 'Deployment is complete' >&2
