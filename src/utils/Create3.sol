2bce6063c2a0756335de472b323edf1cf36c3b38
Duncan Townsend <git@duncancmt.com>
2024-07-17 10:34:12 -0400

    Add London version of `Create3`

--- src/utils/Create3.sol
+++ src/utils/Create3.sol
@@ -48,6 +48,57 @@ import {AddressDerivation} from "./AddressDerivation.sol";
 |       | 1e | 30   | address      | [this 1 cds]                 | {}
 |       | 1f | ff   | selfdestruct | X                            | X
 \-----> | 20 |
+
+
+
+
+
+        // constructor
+        | Address | Bytecode | Mnemonic | Stack                     | Memory
+        ------------------------------------------------------------------
+        | 00 | 7f.. | push32         | [runtime]                    | {}
+        | 21 | 3d   | returndatasize | [0 runtime]                  | {}
+        | 22 | 52   | mstore         | []                           | {runtime}
+/-----< | 23 | 59   | msize          | [32]                         | {runtime}
+|       | 24 | 3d   | returndatasize | [0 32]                       | {runtime}
+|       | 25 | f3   | return         | X                            | X
+|       | 26 |
+|
+|       // runtime
+|       | Address | Bytecode | Mnemonic | Stack                     | Memory
+|       ------------------------------------------------------------------
+|       | 00 | 36   | calldatasize   | [cds]                        | {}
+|       | 01 | 58   | pc             | [1 cds]                      | {}
+|       | 02 | 3d   | returndatasize | [0 1 cds]                    | {}
+|       | 03 | 54   | sload          | [initialized 1 cds]          | {}
+| /---< | 04 | 601d | push1 0x1d     | [target initialized 1 cds]   | {}
+| |     | 06 | 57   | jumpi          | [1 cds]                      | {}
+| |     | 07 | 3d   | returndatasize | [0 1 cds]                    | {}
+| |     | 08 | 55   | sstore         | [cds]                        | {}
+| |     | 09 | 3d   | returndatasize | [0 cds]                      | {}
+| |     | 0a | 3d   | returndatasize | [0 0 cds]                    | {}
+| |     | 0b | 37   | calldatacopy   | []                           | {initCode}
+| |     | 0c | 36   | calldatasize   | [cds]                        | {initCode}
+| |     | 0d | 3d   | returndatasize | [0 cds]                      | {initCode}
+| |     | 0e | 34   | callvalue      | [msg.value 0 cds]            | {initCode}
+| |     | 0f | f0   | create         | [deployed]                   | {initCode}
+| |     | 10   3d   | returndatasize | [0 deployed]                 | {initCode}
+| |     | 11 | 81   | dup2           | [deployed 0 deployed]        | {initCode}
+| | /-< | 12 | 6017 | push1 0x17     | [target deployed 0 deployed] | {initCode}
+| | |   | 14 | 57   | jumpi          | [0 deployed]                 | {initCode}
+| | |   | 15 | 3d   | returndatasize | [0 0 deployed]               | {initCode}
+| | |   | 16 | fd   | revert         | X                            | X
+| | \-> | 17 | 5b   | jumpdest       | [0 deployed]                 | {initCode}
+| |     | 18 | 52   | mstore         | []                           | {deployed ...}
+| |     | 19 | 6020 | push1 0x20     | [32]                         | {deployed ...}
+| |     | 1b | 3d   | returndatasize | [0 32]                       | {deployed ...}
+| |     | 1c | f3   | return         | X                            | X
+| \---> | 1d | 5b   | jumpdest       | [1 cds]                      | {}
+|       | 1e | 30   | address        | [this 1 cds]                 | {}
+|       | 1f | ff   | selfdestruct   | X                            | X
+\-----> | 20 |
+
+
 */
 
 library Create3 {
@@ -57,13 +108,17 @@ library Create3 {
     uint8 private constant _SHIM_LENGTH = 0x26;
     bytes32 private constant _SHIM_INITHASH = 0x3bf3f97f0be1e2c00023033eefeb4fc062ac552ff36778b17060d90b6764902f;
 
-    function createFromCalldata(bytes32 salt, bytes calldata initCode, uint256 value)
-        internal
+    uint256 private constant _SHIM0_LONDON = 0x7f36583d54601d573d553d3d37363d34f03d816017573dfd5b5260203df35b30;
+    uint48 private constant _SHIM1_LONDON = 0xff3d52593df3;
+    bytes32 private constant _SHIM_INITHASH_LONDON = 0x4181fd95643bb6bf1be20faa449de3be679a53ec38d829a0a789397a5d5d4887;
+
+    function _createFromCalldata(bytes32 salt, bytes calldata initCode, uint256 value, uint256 shim0, uint48 shim1)
+        private
         returns (address deployed)
     {
         assembly ("memory-safe") {
-            mstore(_SHIM1_LENGTH, _SHIM1)
-            mstore(0x00, _SHIM0)
+            mstore(_SHIM1_LENGTH, shim1)
+            mstore(0x00, shim0)
             let shim := create2(0x00, 0x00, _SHIM_LENGTH, salt)
             if iszero(shim) { revert(0x00, 0x00) }
             let ptr := mload(0x40)
@@ -79,14 +134,32 @@ library Create3 {
         }
     }
 
+    function createFromCalldata(bytes32 salt, bytes calldata initCode, uint256 value) internal returns (address) {
+        return _createFromCalldata(salt, initCode, value, _SHIM0, _SHIM1);
+    }
+
+    function createFromCalldataLondon(bytes32 salt, bytes calldata initCode, uint256 value)
+        internal
+        returns (address)
+    {
+        return _createFromCalldata(salt, initCode, value, _SHIM0_LONDON, _SHIM1_LONDON);
+    }
+
     function createFromCalldata(bytes32 salt, bytes calldata initCode) internal returns (address) {
         return createFromCalldata(salt, initCode, 0);
     }
 
-    function createFromMemory(bytes32 salt, bytes memory initCode, uint256 value) internal returns (address deployed) {
+    function createFromCalldataLondon(bytes32 salt, bytes calldata initCode) internal returns (address) {
+        return createFromCalldataLondon(salt, initCode, 0);
+    }
+
+    function _createFromMemory(bytes32 salt, bytes memory initCode, uint256 value, uint256 shim0, uint48 shim1)
+        private
+        returns (address deployed)
+    {
         assembly ("memory-safe") {
-            mstore(_SHIM1_LENGTH, _SHIM1)
-            mstore(0x00, _SHIM0)
+            mstore(_SHIM1_LENGTH, shim1)
+            mstore(0x00, shim0)
             let shim := create2(0x00, 0x00, _SHIM_LENGTH, salt)
             if iszero(shim) { revert(0x00, 0x00) }
             if iszero(call(gas(), shim, value, add(0x20, initCode), mload(initCode), 0x00, 0x20)) { revert(0x00, 0x00) }
@@ -95,17 +168,40 @@ library Create3 {
         }
     }
 
+    function createFromMemory(bytes32 salt, bytes memory initCode, uint256 value) internal returns (address) {
+        return _createFromMemory(salt, initCode, value, _SHIM0, _SHIM1);
+    }
+
+    function createFromMemoryLondon(bytes32 salt, bytes memory initCode, uint256 value) internal returns (address) {
+        return _createFromMemory(salt, initCode, value, _SHIM0_LONDON, _SHIM1_LONDON);
+    }
+
     function createFromMemory(bytes32 salt, bytes memory initCode) internal returns (address) {
         return createFromMemory(salt, initCode, 0);
     }
 
+    function createFromMemoryLondon(bytes32 salt, bytes memory initCode) internal returns (address) {
+        return createFromMemory(salt, initCode, 0);
+    }
+
+    function _predict(bytes32 salt, address deployer, bytes32 initHash) private pure returns (address) {
+        return
+            AddressDerivation.deriveContract(AddressDerivation.deriveDeterministicContract(deployer, salt, initHash), 1);
+    }
+
     function predict(bytes32 salt, address deployer) internal pure returns (address) {
-        return AddressDerivation.deriveContract(
-            AddressDerivation.deriveDeterministicContract(deployer, salt, _SHIM_INITHASH), 1
-        );
+        return _predict(salt, deployer, _SHIM_INITHASH);
+    }
+
+    function predictLondon(bytes32 salt, address deployer) internal pure returns (address) {
+        return _predict(salt, deployer, _SHIM_INITHASH_LONDON);
     }
 
     function predict(bytes32 salt) internal view returns (address) {
         return predict(salt, address(this));
+    }
+
+    function predictLondon(bytes32 salt) internal view returns (address) {
+        return predictLondon(salt, address(this));
     }
 }
