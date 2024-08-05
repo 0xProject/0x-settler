3fcbd7ce201c79b02f8bdc89e497091ba0f9bc6e
Duncan Townsend <git@duncancmt.com>
2024-08-05 11:18:22 -0400

    Don't use -z in Bash scripts with -u set

--- sh/deploy_new_chain.sh
+++ sh/deploy_new_chain.sh
@@ -166,7 +166,7 @@ declare rpc_url
 rpc_url="$(get_api_secret rpcUrl)"
 declare -r rpc_url
 
-if [[ -z $rpc_url ]] ; then
+if [[ ${rpc_url:-unset} = 'unset' ]] ; then
     echo '`rpcUrl` is unset in `api_secrets.json` for chain "'"$chain_name"'"' >&2
     exit 1
 fi
