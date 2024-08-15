74e9397550cfbf8dda93d77ed45590f5da824ed7
Duncan Townsend <git@duncancmt.com>
2024-08-15 16:01:02 -0400

    Configure gas/yield on implementation because I don't understand how it works

--- src/deployer/BlastDeployer.sol
+++ src/deployer/BlastDeployer.sol
@@ -6,7 +6,10 @@ import {BLAST, BlastYieldMode, BlastGasMode} from "../chains/IBlast.sol";
 
 /// @custom:security-contact security@0x.org
 contract BlastDeployer is Deployer {
-    constructor(uint256 version) Deployer(version) {}
+    constructor(uint256 version) Deployer(version) {
+        assert(block.chainid == 81457);
+        BLAST.configure(BlastYieldMode.AUTOMATIC, BlastGasMode.CLAIMABLE, BlastDeployer(0x00000000000004533Fe15556B1E086BB1A72cEae).owner());
+    }
 
     function initialize(address initialOwner) public override {
         assert(block.chainid == 81457);
