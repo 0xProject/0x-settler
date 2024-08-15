22b8f23b94728f96ab9b8548697e8e9e37ed08a7
Duncan Townsend <git@duncancmt.com>
2024-08-15 12:33:33 -0400

    Add `DODOV2` action on Arbitrum

--- src/chains/Arbitrum.sol
+++ src/chains/Arbitrum.sol
@@ -8,6 +8,7 @@ import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {MaverickV2, IMaverickV2Pool} from "../core/MaverickV2.sol";
 import {CurveTricrypto} from "../core/CurveTricrypto.sol";
+import {DodoV2, IDodoV2} from "../core/DodoV2.sol";
 import {FreeMemory} from "../utils/FreeMemory.sol";
 
 import {ISettlerActions} from "../ISettlerActions.sol";
@@ -36,7 +37,7 @@ import {SettlerAbstract} from "../SettlerAbstract.sol";
 import {AbstractContext} from "../Context.sol";
 import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
 
-abstract contract ArbitrumMixin is FreeMemory, SettlerBase, MaverickV2, CurveTricrypto {
+abstract contract ArbitrumMixin is FreeMemory, SettlerBase, MaverickV2, CurveTricrypto, DodoV2 {
     constructor() {
         assert(block.chainid == 42161 || block.chainid == 31337);
     }
@@ -61,6 +62,11 @@ abstract contract ArbitrumMixin is FreeMemory, SettlerBase, MaverickV2, CurveTri
             ) = abi.decode(data, (address, IERC20, uint256, IMaverickV2Pool, bool, uint256));
 
             sellToMaverickV2(recipient, sellToken, bps, pool, tokenAIn, minBuyAmount);
+        } else if (action == ISettlerActions.DODOV2.selector) {
+            (address recipient, IERC20 sellToken, uint256 bps, IDodoV2 dodo, bool quoteForBase, uint256 minBuyAmount) =
+                abi.decode(data, (address, IERC20, uint256, IDodoV2, bool, uint256));
+
+            sellToDodoV2(recipient, sellToken, bps, dodo, quoteForBase, minBuyAmount);
         } else {
             return false;
         }
