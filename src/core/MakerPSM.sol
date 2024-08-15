407bfae7865c468f2e2eecfeb6cd9f6e88533554
Duncan Townsend <git@duncancmt.com>
2024-08-15 10:55:51 -0400

    Add slippage checking to `MAKERPSM` action -- only supported on the "lite" PSM

--- src/core/MakerPSM.sol
+++ src/core/MakerPSM.sol
@@ -40,7 +40,7 @@ abstract contract MakerPSM {
         assert(block.chainid == 1 || block.chainid == 31337);
     }
 
-    function sellToMakerPsm(address recipient, IERC20 gemToken, uint256 bps, IPSM psm, bool buyGem) internal {
+    function sellToMakerPsm(address recipient, IERC20 gemToken, uint256 bps, IPSM psm, bool buyGem, uint256 amountOutMin) internal {
         if (buyGem) {
             // phantom overflow can't happen here because DAI has decimals = 18
             uint256 sellAmount = (DAI.balanceOf(address(this)) * bps).unsafeDiv(10_000);
@@ -48,6 +48,9 @@ abstract contract MakerPSM {
                 uint256 feeDivisor = psm.tout() + WAD; // eg. 1.001 * 10 ** 18 with 0.1% fee [tout is in wad];
                 // overflow can't happen at all because DAI is reasonable and PSM prohibits gemToken with decimals > 18
                 uint256 buyAmount = (sellAmount * 10 ** uint256(gemToken.decimals())).unsafeDiv(feeDivisor);
+                if (buyAmount < amountOutMin) {
+                    revert TooMuchSlippage(gemToken, amountOutMin, buyAmount);
+                }
 
                 DAI.safeApproveIfBelow(address(psm), sellAmount);
                 psm.buyGem(recipient, buyAmount);
@@ -57,6 +60,20 @@ abstract contract MakerPSM {
             uint256 sellAmount = (gemToken.balanceOf(address(this)) * bps).unsafeDiv(10_000);
             gemToken.safeApproveIfBelow(psm.gemJoin(), sellAmount);
             psm.sellGem(recipient, sellAmount);
+            if (amountOutMin != 0) {
+                uint256 buyAmount;
+                assembly ("memory-safe") {
+                    // `returndatacopy` causes an exceptional revert if there's an out-of-bounds access.
+                    // "LitePSM USDC A" (0xf6e72Db5454dd049d0788e411b06CfAF16853042) returns the amount out
+                    // "MCD PSM USDC A" (0x89B78CfA322F6C5dE0aBcEecab66Aee45393cC5A) returns nothing
+                    // When interacting with "MCD PSM USDC A", `amountOutMin` must be zero
+                    returndatacopy(0x00, 0x00, 0x20)
+                    buyAmount := mload(0x00)
+                }
+                if (buyAmount < amountOutMin) {
+                    revert TooMuchSlippage(DAI, amountOutMin, buyAmount);
+                }
+            }
         }
     }
 }
