3d5ae8d1d7bfd27f2a0a7c06a538e3f93d6ab4d1
Duncan Townsend <git@duncancmt.com>
2024-08-22 13:38:09 -0400

    Fix unit test

--- test/unit/core/MakerPSMUnitTest.t.sol
+++ test/unit/core/MakerPSMUnitTest.t.sol
@@ -8,7 +8,118 @@ import {Utils} from "../Utils.sol";
 
 import {Test} from "forge-std/Test.sol";
 
+import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+
 contract MakerPSMDummy is MakerPSM {
+    function _msgSender() internal pure override returns (address) {
+        revert("unimplemented");
+    }
+
+    function _msgData() internal pure override returns (bytes calldata) {
+        revert("unimplemented");
+    }
+
+    function _isForwarded() internal pure override returns (bool) {
+        revert("unimplemented");
+    }
+
+    function _hasMetaTxn() internal pure override returns (bool) {
+        revert("unimplemented");
+    }
+
+    function _dispatch(uint256, bytes4, bytes calldata) internal pure override returns (bool) {
+        revert("unimplemented");
+    }
+
+    function _isRestrictedTarget(address) internal pure override returns (bool) {
+        revert("unimplemented");
+    }
+
+    function _operator() internal pure override returns (address) {
+        revert("unimplemented");
+    }
+
+    function _permitToSellAmount(ISignatureTransfer.PermitTransferFrom memory)
+        internal
+        pure
+        override
+        returns (uint256 sellAmount)
+    {
+        revert("unimplemented");
+    }
+
+    function _permitToTransferDetails(ISignatureTransfer.PermitTransferFrom memory, address)
+        internal
+        pure
+        override
+        returns (ISignatureTransfer.SignatureTransferDetails memory, uint256)
+    {
+        revert("unimplemented");
+    }
+
+    function _transferFromIKnowWhatImDoing(
+        ISignatureTransfer.PermitTransferFrom memory,
+        ISignatureTransfer.SignatureTransferDetails memory,
+        address,
+        bytes32,
+        string memory,
+        bytes memory,
+        bool
+    ) internal pure override {
+        revert("unimplemented");
+    }
+
+    function _transferFromIKnowWhatImDoing(
+        ISignatureTransfer.PermitTransferFrom memory,
+        ISignatureTransfer.SignatureTransferDetails memory,
+        address,
+        bytes32,
+        string memory,
+        bytes memory
+    ) internal pure override {
+        revert("unimplemented");
+    }
+
+    function _transferFrom(
+        ISignatureTransfer.PermitTransferFrom memory,
+        ISignatureTransfer.SignatureTransferDetails memory,
+        bytes memory,
+        bool
+    ) internal pure override {
+        revert("unimplemented");
+    }
+
+    function _transferFrom(
+        ISignatureTransfer.PermitTransferFrom memory,
+        ISignatureTransfer.SignatureTransferDetails memory,
+        bytes memory
+    ) internal pure override {
+        revert("unimplemented");
+    }
+
+    function _setOperatorAndCall(
+        address,
+        bytes memory,
+        uint32,
+        function (bytes calldata) internal returns (bytes memory)
+    ) internal pure override returns (bytes memory) {
+        revert("unimplemented");
+    }
+
+    modifier metaTx(address, bytes32) override {
+        revert("unimplemented");
+        _;
+    }
+
+    modifier takerSubmitted() override {
+        revert("unimplemented");
+        _;
+    }
+
+    function _allowanceHolderTransferFrom(address, address, address, uint256) internal pure override {
+        revert("unimplemented");
+    }
+
     function sellToPool(address recipient, address gemToken, uint256 bps, address psm) public {
         super.sellToMakerPsm(recipient, IERC20(gemToken), bps, IPSM(psm), false, 0);
     }
