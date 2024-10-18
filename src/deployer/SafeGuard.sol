d7ec5fd0893c9214011f758beb691681c9a171b8
Duncan Townsend <git@duncancmt.com>
2024-10-18 08:50:53 -0400

    Add lockdown mode to Safe Guard (requires unanimous signatures to unlock)

--- src/deployer/SafeGuard.sol
+++ src/deployer/SafeGuard.sol
@@ -20,9 +20,17 @@ interface ISafeMinimal {
         uint256 nonce
     ) external view returns (bytes memory);
 
+    function checkNSignatures(bytes32 dataHash, bytes memory data, bytes memory signatures, uint256 requiredSignatures)
+        external
+        view;
+
     function nonce() external view returns (uint256);
 
     function isOwner(address) external view returns (bool);
+
+    function getStorageAt(uint256 offset, uint256 length) external view returns (bytes memory);
+
+    function approvedHashes(address owner, bytes32 txHash) external view returns (uint256);
 }
 
 library SafeLib {
@@ -30,7 +38,7 @@ library SafeLib {
         ISafeMinimal safe,
         address to,
         uint256 value,
-        bytes calldata data,
+        bytes memory data,
         Operation operation,
         uint256 safeTxGas,
         uint256 baseGas,
@@ -98,20 +106,27 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         uint256 nonce
     );
     event SafeTransactionCanceled(bytes32 indexed txHash);
+    event LockDown(address indexed lockedDownBy, bytes32 indexed unlockTxHash);
+    event Unlocked();
 
     error PermissionDenied();
     error TimelockNotElapsed(bytes32 txHash, uint256 timelockEnd);
     error TimelockElapsed(bytes32 txHash, uint256 timelockEnd);
     error NotQueued(bytes32 txHash);
+    error LockedDown(address lockedDownBy);
+    error NotLockedDown();
+    error UnlockHashedNotApproved(bytes32 txHash);
 
     uint256 public delay;
     mapping(bytes32 => uint256) public timelockEnd;
+    address public lockedDownBy;
 
     ISafeMinimal public immutable safe;
-    bytes32 internal constant evmVersionDummyInitHash = bytes32(0); // TODO: ensure London hardfork
+    bytes32 internal constant _EVM_VERSION_DUMMY_INITHASH = bytes32(0); // TODO: ensure London hardfork
+    uint256 internal constant _OWNER_COUNT_SLOT = 3;
 
     constructor(address _safe) {
-        assert(keccak256(type(EvmVersionDummy).creationCode) == evmVersionDummyInitHash || block.chainid == 31337);
+        assert(keccak256(type(EvmVersionDummy).creationCode) == _EVM_VERSION_DUMMY_INITHASH || block.chainid == 31337);
         safe = ISafeMinimal(_safe);
         delay = 1 weeks;
         emit TimelockUpdated(0, 1 weeks);
@@ -139,6 +154,17 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         _;
     }
 
+    function _requireNotLockedDown() private view {
+        if (lockedDownBy != address(0)) {
+            revert LockedDown(lockedDownBy);
+        }
+    }
+
+    modifier notLockedDown() {
+        _requireNotLockedDown();
+        _;
+    }
+
     function checkTransaction(
         address to,
         uint256 value,
@@ -149,10 +175,18 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         uint256 gasPrice,
         address gasToken,
         address payable refundReceiver,
-        bytes calldata, // signatures
+        bytes calldata signatures,
         address // msgSender
-    ) external view override onlySafe {
+    ) external view override onlySafe /* notLockedDown is on checkAfterExecution */ {
         ISafeMinimal _safe = ISafeMinimal(msg.sender);
+        uint256 nonce;
+        unchecked {
+            nonce = _safe.nonce() - 1;
+        }
+        bytes memory txHashData = _safe.encodeTransactionData(
+            to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
+        );
+        bytes32 txHash = _safe.getTransactionHash(txHashData);
         if (to == address(this)) {
             if (data.length >= 4) {
                 uint256 selector;
@@ -162,15 +196,15 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
                 if (selector == uint32(this.queue.selector)) {
                     return;
                 }
+                if (selector == uint32(this.unlock.selector)) {
+                    uint256 ownerCount = abi.decode(_safe.getStorageAt(_OWNER_COUNT_SLOT, 1), (uint256));
+                    require(!_safe.isOwner(address(this)));
+                    _safe.checkNSignatures(txHash, txHashData, signatures, ownerCount);
+                    return;
+                }
             }
         }
-        uint256 nonce;
-        unchecked {
-            nonce = _safe.nonce() - 1;
-        }
-        bytes32 txHash = _safe.getTransactionHash(
-            to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
-        );
+
         uint256 _timelockEnd = timelockEnd[txHash];
         if (_timelockEnd == 0) {
             revert NotQueued(txHash);
@@ -180,7 +214,7 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         }
     }
 
-    function checkAfterExecution(bytes32 txHash, bool success) external view override onlySafe {}
+    function checkAfterExecution(bytes32, bool) external view override onlySafe notLockedDown {}
 
     function queue(
         address to,
@@ -201,7 +235,18 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         uint256 _timelockEnd = block.timestamp + delay;
         timelockEnd[txHash] = _timelockEnd;
         emit SafeTransactionQueued(
-            txHash, _timelockEnd, to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
+            txHash,
+            _timelockEnd,
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver,
+            nonce
         );
     }
 
@@ -222,5 +267,35 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         }
         delete timelockEnd[txHash];
         emit SafeTransactionCanceled(txHash);
+    }
+
+    function lockDown() external onlyOwner notLockedDown {
+        require(!safe.isOwner(address(this)));
+        uint256 nonce = safe.nonce();
+        bytes32 txHash = safe.getTransactionHash(
+            address(this),
+            0 ether,
+            abi.encodeCall(this.unlock, ()),
+            Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            payable(address(0)),
+            nonce
+        );
+        if (safe.approvedHashes(msg.sender, txHash) != 1) {
+            revert UnlockHashedNotApproved(txHash);
+        }
+        lockedDownBy = msg.sender;
+        emit LockDown(msg.sender, txHash);
+    }
+
+    function unlock() external onlySafe {
+        if (lockedDownBy == address(0)) {
+            revert NotLockedDown();
+        }
+        delete lockedDownBy;
+        emit Unlocked();
     }
 }
