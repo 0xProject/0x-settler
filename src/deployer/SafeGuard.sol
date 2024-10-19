0769fa8afb76c09ebc4a8154cc37dcb2dea04c8b
Duncan Townsend <git@duncancmt.com>
2024-10-19 16:27:27 -0400

    Rework Safe guard so that it only requires 1 multisig transaction; this improves the UX of using the guard
    It still requires 2 transactions to be submitted. There's no way around that.

--- src/deployer/SafeGuard.sol
+++ src/deployer/SafeGuard.sol
@@ -7,35 +7,11 @@ enum Operation {
 }
 
 interface ISafeMinimal {
-    function encodeTransactionData(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Operation operation,
-        uint256 safeTxGas,
-        uint256 baseGas,
-        uint256 gasPrice,
-        address gasToken,
-        address payable refundReceiver,
-        uint256 nonce
-    ) external view returns (bytes memory);
-
-    function getTransactionHash(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Operation operation,
-        uint256 safeTxGas,
-        uint256 baseGas,
-        uint256 gasPrice,
-        address gasToken,
-        address payable refundReceiver,
-        uint256 nonce
-    ) external view returns (bytes32);
-
     function checkNSignatures(bytes32 dataHash, bytes memory data, bytes memory signatures, uint256 requiredSignatures)
         external
         view;
+
+    function checkSignatures(bytes32 dataHash, bytes memory data, bytes memory signatures) external view;
 
     function nonce() external view returns (uint256);
 
@@ -44,6 +20,94 @@ interface ISafeMinimal {
     function getStorageAt(uint256 offset, uint256 length) external view returns (bytes memory);
 
     function approvedHashes(address owner, bytes32 txHash) external view returns (uint256);
+}
+
+library SafeLib {
+    function encodeTransactionData(
+        ISafeMinimal safe,
+        address to,
+        uint256 value,
+        bytes memory data,
+        Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        uint256 nonce
+    ) internal view returns (bytes memory) {
+        bytes32 safeTxHash = keccak256(
+            abi.encode(
+                SAFE_TX_TYPEHASH(safe),
+                to,
+                value,
+                keccak256(data),
+                operation,
+                safeTxGas,
+                baseGas,
+                gasPrice,
+                gasToken,
+                refundReceiver,
+                nonce
+            )
+        );
+        return abi.encodePacked(hex"1901", domainSeparator(safe), safeTxHash);
+    }
+
+    function getTransactionHash(
+        ISafeMinimal safe,
+        address to,
+        uint256 value,
+        bytes memory data,
+        Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        uint256 nonce
+    ) internal view returns (bytes32) {
+        return getTransactionHash(
+            safe,
+            encodeTransactionData(
+                safe, to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
+            )
+        );
+    }
+
+    function getTransactionHash(ISafeMinimal, bytes memory txHashData) internal pure returns (bytes32) {
+        return keccak256(txHashData);
+    }
+
+    uint256 private constant _OWNER_COUNT_SLOT = 3;
+
+    function ownerCount(ISafeMinimal safe) internal view returns (uint256) {
+        return abi.decode(safe.getStorageAt(_OWNER_COUNT_SLOT, 1), (uint256));
+    }
+
+    function DOMAIN_SEPARATOR_TYPEHASH(ISafeMinimal) internal pure returns (bytes32) {
+        // keccak256(
+        //     "EIP712Domain(uint256 chainId,address verifyingContract)"
+        // );
+        return 0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
+    }
+
+    function domainSeparator(ISafeMinimal safe) internal view returns (bytes32) {
+        return keccak256(abi.encode(DOMAIN_SEPARATOR_TYPEHASH(safe), block.chainid, safe));
+    }
+
+    function SAFE_TX_TYPEHASH(ISafeMinimal) internal pure returns (bytes32) {
+        // keccak256(
+        //     "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+        // );
+        return 0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8;
+    }
+
+    uint256 private constant _SINGLETON_SLOT = 0;
+
+    function singleton(ISafeMinimal safe) internal view returns (address) {
+        return abi.decode(safe.getStorageAt(_SINGLETON_SLOT, 1), (address));
+    }
 }
 
 interface IGuard {
@@ -74,6 +138,8 @@ contract EvmVersionDummy {
 }
 
 contract ZeroExSettlerDeployerSafeGuard is IGuard {
+    using SafeLib for ISafeMinimal;
+
     event TimelockUpdated(uint256 oldDelay, uint256 newDelay);
     event SafeTransactionEnqueued(
         bytes32 indexed txHash,
@@ -87,7 +153,8 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         uint256 gasPrice,
         address gasToken,
         address payable refundReceiver,
-        uint256 nonce
+        uint256 nonce,
+        bytes signatures
     );
     event SafeTransactionCanceled(bytes32 indexed txHash);
     event LockDown(address indexed lockedDownBy, bytes32 indexed unlockTxHash);
@@ -101,6 +168,7 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
     error LockedDown(address lockedDownBy);
     error NotLockedDown();
     error UnlockHashNotApproved(bytes32 txHash);
+    error UnexpectedUpgrade(address newSingleton);
 
     struct NextTransaction {
         bytes32 txHash;
@@ -108,12 +176,12 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
     }
 
     uint256 public delay;
-    NextTransaction public nextTransaction;
+    mapping(bytes32 => uint256) public timelockEnd;
     address public lockedDownBy;
 
     ISafeMinimal public immutable safe;
     bytes32 internal constant _EVM_VERSION_DUMMY_INITHASH = bytes32(0); // TODO: ensure London hardfork
-    uint256 internal constant _OWNER_COUNT_SLOT = 3;
+    address internal constant _SINGLETON = 0xfb1bffC9d739B8D520DaF37dF666da4C687191EA;
 
     constructor(address _safe, uint256 _delay) {
         assert(keccak256(type(EvmVersionDummy).creationCode) == _EVM_VERSION_DUMMY_INITHASH || block.chainid == 31337);
@@ -173,22 +241,28 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         address payable refundReceiver,
         bytes calldata signatures,
         address // msgSender
-    ) external override onlySafe {
+    ) external view override onlySafe {
         ISafeMinimal _safe = ISafeMinimal(msg.sender);
 
-        // There are 2 special cases to handle (transactions that don't require doing through the
-        // timelock). A transaction that enqueues another transaction, and unlocking after a
-        // lockdown.
+        // The nonce has already been incremented past the value used in the
+        // currently-executing transaction. We decrement it to get the value that was hashed
+        // to get the `txHash`.
+        uint256 nonce = _safe.nonce() - 1;
+
+        // `txHashData` is used here for an outdated, nonstandard variant of nested ERC1271
+        // signatures that passes the signing hash as `bytes` instead of as `bytes32`. This only
+        // matters for the `checkNSignatures` call when validating before `unlock()`.
+        bytes memory txHashData = _safe.encodeTransactionData(
+            to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
+        );
+        bytes32 txHash = _safe.getTransactionHash(txHashData);
+
+        // The call to `this.unlock()` is special-cased.
         if (to == address(this)) {
             if (operation != Operation.Call) {
                 revert NoDelegateToGuard();
             }
-            uint256 selector = uint32(bytes4(data));
-            if (selector == uint32(this.enqueue.selector)) {
-                _requireNotLockedDown();
-                return;
-            }
-            if (selector == uint32(this.unlock.selector)) {
+            if (uint256(uint32(bytes4(data))) == uint256(uint32(this.unlock.selector))) {
                 // A call to `unlock` does not go through the timelock, but we require additional
                 // signatures in order for the lockdown functionality to be effective, as a
                 // protocol.
@@ -199,55 +273,54 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
                 _requireLockedDown();
 
                 // Calling `unlock()` requires unanimous signatures, i.e. a threshold equal to the
-                // owner count. The owner who called `lockDown()` has already signed (to prevent
-                // griefing).
-                uint256 ownerCount = abi.decode(_safe.getStorageAt(_OWNER_COUNT_SLOT, 1), (uint256));
-
-                // The nonce has already been incremented past the value used in the
-                // currently-executing transaction. We decrement it to get the value that was hashed
-                // to get the `txHash`.
-                uint256 nonce = _safe.nonce() - 1;
-
-                // `txHashData` is used here for an outdated, nonstandard variant of nested ERC1271
-                // signatures that passes the signing hash as `bytes` instead of as
-                // `bytes32`. `txHash == keccak256(txHashData)`, but we would rather not make
-                // assumptions about the relationship than gas optimize. Perhaps in some bizarre
-                // world, the Safe may be upgraded so that this relationship no longer holds.
-                bytes32 txHash = _safe.getTransactionHash(
-                    to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
-                );
-                bytes memory txHashData = _safe.encodeTransactionData(
-                    to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
-                );
+                // owner count. We go beyond the usual requirement of just the threshold. The owner
+                // who called `lockDown()` has already signed (to prevent griefing).
+                uint256 ownerCount = _safe.ownerCount();
                 _safe.checkNSignatures(txHash, txHashData, signatures, ownerCount);
 
                 return;
             }
         }
+        // Fall through to the "normal" case, where we're doing anything except calling
+        // `this.unlock()`. The checks that need to be performed here are 1) that the Safe is not
+        // locked down, 2) that the transaction was previously queued through `enqueue` and 3) that
+        // `delay` has elapsed since `enqueue` was called.
 
-        // We're not in either of the 2 special cases. This is the "normal" case. The checks that
-        // need to be performed here are 1) that the safe is not locked down, 2) that the
-        // transaction was previously queued through `enqueue` and 3) that `delay` has elapsed since
-        // `enqueue` was called.
-        {
-            _requireNotLockedDown();
-            uint256 nonce = _safe.nonce() - 1;
-            bytes32 txHash = _safe.getTransactionHash(
-                to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
-            );
-            if (txHash != nextTransaction.txHash) {
-                revert NotQueued(txHash);
-            }
-            uint256 timelockEnd = nextTransaction.timelockEnd;
-            if (block.timestamp <= timelockEnd) {
-                revert TimelockNotElapsed(txHash, timelockEnd);
-            }
+        // We check that the Safe is not locked down twice. We have to check it here to ensure that
+        // we're not smuggling a call to `unlock()` through (e.g.) a delegatecall to
+        // MultiSendCallOnly.
+        _requireNotLockedDown();
+
+        uint256 _timelockEnd = timelockEnd[txHash];
+
+        if (_timelockEnd == 0) {
+            revert NotQueued(txHash);
         }
-
-        delete nextTransaction;
+        if (block.timestamp <= _timelockEnd) {
+            revert TimelockNotElapsed(txHash, _timelockEnd);
+        }
     }
 
-    function checkAfterExecution(bytes32, bool) external view override onlySafe {}
+    // We check that the Safe is not locked down twice. We have to check it here to ensure that the
+    // call to `unlock()` can't revert and burn signatures (increase the nonce), resulting in a
+    // bricked/griefing attack by a malicious owner.
+    function checkAfterExecution(bytes32, bool) external view override onlySafe notLockedDown {
+        ISafeMinimal _safe = ISafeMinimal(msg.sender);
+
+        // Due to a quirk of how `checkNSignatures` works (called as a precondition to `unlock`;
+        // sometimes it validates `msg.sender` instead of a signature, for gas optimization), we
+        // could end up in a bizarre situation if `address(this)` is an owner. This would make our
+        // introspection checks wrong. Let's just prohibit that entirely.
+        require(!_safe.isOwner(address(this)));
+
+        // A malicious upgrade could lie to us about this. We're not trying to prevent that. We're
+        // trying to prevent an unexpected upgrade that may break our ability to reliably introspect
+        // the aspects of the Safe that are required for the correct function of this guard.
+        address singleton = _safe.singleton();
+        if (singleton != _SINGLETON) {
+            revert UnexpectedUpgrade(singleton);
+        }
+    }
 
     function enqueue(
         address to,
@@ -258,21 +331,22 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         uint256 baseGas,
         uint256 gasPrice,
         address gasToken,
-        address payable refundReceiver
-    ) external onlySafe {
-        ISafeMinimal _safe = ISafeMinimal(msg.sender);
-
-        uint256 nonce = _safe.nonce();
-        bytes32 txHash = _safe.getTransactionHash(
+        address payable refundReceiver,
+        uint256 nonce,
+        bytes calldata signatures
+    ) external {
+        bytes memory txHashData = safe.encodeTransactionData(
             to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, nonce
         );
+        bytes32 txHash = safe.getTransactionHash(txHashData);
+        safe.checkSignatures(txHash, txHashData, signatures);
 
-        uint256 timelockEnd = block.timestamp + delay;
-        (nextTransaction.txHash, nextTransaction.timelockEnd) = (txHash, timelockEnd);
+        uint256 _timelockEnd = block.timestamp + delay;
+        timelockEnd[txHash] = _timelockEnd;
 
         emit SafeTransactionEnqueued(
             txHash,
-            timelockEnd,
+            _timelockEnd,
             to,
             value,
             data,
@@ -282,11 +356,12 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
             gasPrice,
             gasToken,
             refundReceiver,
-            nonce
+            nonce,
+            signatures
         );
     }
 
-    /// It's totally possible to brick the timelock (and consequently, the entire safe) if you set
+    /// It's totally possible to brick the timelock (and consequently, the entire Safe) if you set
     /// the delay too long. Don't do that.
     function setDelay(uint256 newDelay) external onlySafe {
         emit TimelockUpdated(delay, newDelay);
@@ -294,18 +369,18 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
     }
 
     function cancel(bytes32 txHash) external onlyOwner {
-        if (txHash != nextTransaction.txHash) {
+        uint256 _timelockEnd = timelockEnd[txHash];
+        if (_timelockEnd == 0) {
             revert NotQueued(txHash);
         }
-        uint256 timelockEnd = nextTransaction.timelockEnd;
-        if (block.timestamp > timelockEnd) {
-            revert TimelockElapsed(txHash, timelockEnd);
+        if (block.timestamp > _timelockEnd) {
+            revert TimelockElapsed(txHash, _timelockEnd);
         }
-        delete nextTransaction;
+        timelockEnd[txHash] = type(uint256).max;
         emit SafeTransactionCanceled(txHash);
     }
 
-    function lockDownTxHash() public view returns (bytes32) {
+    function lockDownTxHash() public view notLockedDown returns (bytes32) {
         uint256 nonce = safe.nonce();
         return safe.getTransactionHash(
             address(this),
@@ -321,13 +396,7 @@ contract ZeroExSettlerDeployerSafeGuard is IGuard {
         );
     }
 
-    function lockDown() external onlyOwner notLockedDown {
-        // Due to a quirk of how `checkNSignatures` works (called as a precondition to `unlock`;
-        // sometimes it validates `msg.sender` instead of a signature, for gas optimization), we
-        // could end up in a bizarre situation if `address(this)` is an owner. Let's just prohibit
-        // that entirely.
-        require(!safe.isOwner(address(this)));
-
+    function lockDown() external onlyOwner {
         bytes32 txHash = lockDownTxHash();
 
         // By requiring that the locker has preapproved the `txHash` for the call to `unlock`, we
