242af5f21228edc2836b898c0a36914eea750f31
Duncan Townsend <git@duncancmt.com>
2024-12-12 06:18:05 -0500

    WIP:

--- src/chains/Sepolia/Common.sol
+++ src/chains/Sepolia/Common.sol
@@ -1,29 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.25;
 
-import {SettlerBase} from "../SettlerBase.sol";
-import {Settler} from "../Settler.sol";
-import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
+import {SettlerBase} from "../../SettlerBase.sol";
 
 import {IERC20} from "@forge-std/interfaces/IERC20.sol";
-import {MaverickV2, IMaverickV2Pool} from "../core/MaverickV2.sol";
-import {FreeMemory} from "../utils/FreeMemory.sol";
+import {MaverickV2, IMaverickV2Pool} from "../../core/MaverickV2.sol";
+import {FreeMemory} from "../../utils/FreeMemory.sol";
 
-import {ISettlerActions} from "../ISettlerActions.sol";
+import {ISettlerActions} from "../../ISettlerActions.sol";
 import {ISignatureTransfer} from "@permit2/interfaces/ISignatureTransfer.sol";
-import {UnknownForkId} from "../core/SettlerErrors.sol";
+import {UnknownForkId} from "../../core/SettlerErrors.sol";
 
 import {
     uniswapV3SepoliaFactory,
     uniswapV3InitHash,
     uniswapV3ForkId,
     IUniswapV3Callback
-} from "../core/univ3forks/UniswapV3.sol";
+} from "../../core/univ3forks/UniswapV3.sol";
 
 // Solidity inheritance is stupid
-import {SettlerAbstract} from "../SettlerAbstract.sol";
-import {AbstractContext} from "../Context.sol";
-import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
+import {SettlerAbstract} from "../../SettlerAbstract.sol";
 
 abstract contract SepoliaMixin is FreeMemory, SettlerBase, MaverickV2 {
     constructor() {
@@ -69,94 +65,5 @@ abstract contract SepoliaMixin is FreeMemory, SettlerBase, MaverickV2 {
         } else {
             revert UnknownForkId(forkId);
         }
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract SepoliaSettler is Settler, SepoliaMixin {
-    constructor(bytes20 gitCommit) Settler(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data) internal override DANGEROUS_freeMemory returns (bool) {
-        if (super._dispatchVIP(action, data)) {
-            return true;
-        } else if (action == uint32(ISettlerActions.MAVERICKV2_VIP.selector)) {
-            (
-                address recipient,
-                bytes32 salt,
-                bool tokenAIn,
-                ISignatureTransfer.PermitTransferFrom memory permit,
-                bytes memory sig,
-                uint256 minBuyAmount
-            ) = abi.decode(data, (address, bytes32, bool, ISignatureTransfer.PermitTransferFrom, bytes, uint256));
-
-            sellToMaverickV2VIP(recipient, salt, tokenAIn, permit, sig, minBuyAmount);
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    // Solidity inheritance is stupid
-    function _isRestrictedTarget(address target)
-        internal
-        pure
-        override(Settler, Permit2PaymentAbstract)
-        returns (bool)
-    {
-        return super._isRestrictedTarget(target);
-    }
-
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, SepoliaMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(Settler, AbstractContext) returns (address) {
-        return super._msgSender();
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract SepoliaSettlerMetaTxn is SettlerMetaTxn, SepoliaMixin {
-    constructor(bytes20 gitCommit) SettlerMetaTxn(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data, bytes calldata sig)
-        internal
-        override
-        DANGEROUS_freeMemory
-        returns (bool)
-    {
-        if (super._dispatchVIP(action, data, sig)) {
-            return true;
-        } else if (action == uint32(ISettlerActions.METATXN_MAVERICKV2_VIP.selector)) {
-            (
-                address recipient,
-                bytes32 salt,
-                bool tokenAIn,
-                ISignatureTransfer.PermitTransferFrom memory permit,
-                uint256 minBuyAmount
-            ) = abi.decode(data, (address, bytes32, bool, ISignatureTransfer.PermitTransferFrom, uint256));
-
-            sellToMaverickV2VIP(recipient, salt, tokenAIn, permit, sig, minBuyAmount);
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, SepoliaMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(SettlerMetaTxn, AbstractContext) returns (address) {
-        return super._msgSender();
     }
 }
