9822a2cd6b8386b7fdc67506b58382292b42e436
Duncan Townsend <git@duncancmt.com>
2024-12-12 06:14:37 -0500

    WIP:

--- src/chains/Polygon/Common.sol
+++ src/chains/Polygon/Common.sol
@@ -1,32 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.25;
 
-import {SettlerBase} from "../SettlerBase.sol";
-import {Settler} from "../Settler.sol";
-import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
+import {SettlerBase} from "../../SettlerBase.sol";
 
 import {IERC20} from "@forge-std/interfaces/IERC20.sol";
-import {DodoV1, IDodoV1} from "../core/DodoV1.sol";
-import {DodoV2, IDodoV2} from "../core/DodoV2.sol";
-import {FreeMemory} from "../utils/FreeMemory.sol";
+import {DodoV1, IDodoV1} from "../../core/DodoV1.sol";
+import {DodoV2, IDodoV2} from "../../core/DodoV2.sol";
+import {FreeMemory} from "../../utils/FreeMemory.sol";
 
-import {ISettlerActions} from "../ISettlerActions.sol";
-import {UnknownForkId} from "../core/SettlerErrors.sol";
+import {ISettlerActions} from "../../ISettlerActions.sol";
+import {UnknownForkId} from "../../core/SettlerErrors.sol";
 
 import {
     uniswapV3MainnetFactory,
     uniswapV3InitHash,
     uniswapV3ForkId,
     IUniswapV3Callback
-} from "../core/univ3forks/UniswapV3.sol";
-import {IAlgebraCallback} from "../core/univ3forks/Algebra.sol";
-import {sushiswapV3PolygonFactory, sushiswapV3ForkId} from "../core/univ3forks/SushiswapV3.sol";
-import {quickSwapV3Factory, quickSwapV3InitHash, quickSwapV3ForkId} from "../core/univ3forks/QuickSwapV3.sol";
+} from "../../core/univ3forks/UniswapV3.sol";
+import {IAlgebraCallback} from "../../core/univ3forks/Algebra.sol";
+import {sushiswapV3PolygonFactory, sushiswapV3ForkId} from "../../core/univ3forks/SushiswapV3.sol";
+import {quickSwapV3Factory, quickSwapV3InitHash, quickSwapV3ForkId} from "../../core/univ3forks/QuickSwapV3.sol";
 
 // Solidity inheritance is stupid
-import {SettlerAbstract} from "../SettlerAbstract.sol";
-import {AbstractContext} from "../Context.sol";
-import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
+import {SettlerAbstract} from "../../SettlerAbstract.sol";
 
 abstract contract PolygonMixin is FreeMemory, SettlerBase, DodoV1, DodoV2 {
     constructor() {
@@ -79,63 +75,5 @@ abstract contract PolygonMixin is FreeMemory, SettlerBase, DodoV1, DodoV2 {
         } else {
             revert UnknownForkId(forkId);
         }
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract PolygonSettler is Settler, PolygonMixin {
-    constructor(bytes20 gitCommit) Settler(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data) internal override DANGEROUS_freeMemory returns (bool) {
-        return super._dispatchVIP(action, data);
-    }
-
-    // Solidity inheritance is stupid
-    function _isRestrictedTarget(address target)
-        internal
-        pure
-        override(Settler, Permit2PaymentAbstract)
-        returns (bool)
-    {
-        return super._isRestrictedTarget(target);
-    }
-
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, PolygonMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(Settler, AbstractContext) returns (address) {
-        return super._msgSender();
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract PolygonSettlerMetaTxn is SettlerMetaTxn, PolygonMixin {
-    constructor(bytes20 gitCommit) SettlerMetaTxn(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data, bytes calldata sig)
-        internal
-        override
-        DANGEROUS_freeMemory
-        returns (bool)
-    {
-        return super._dispatchVIP(action, data, sig);
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, PolygonMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(SettlerMetaTxn, AbstractContext) returns (address) {
-        return super._msgSender();
     }
 }
