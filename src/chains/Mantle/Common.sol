45976733d6df768677efece2d812f016869f2d3f
Duncan Townsend <git@duncancmt.com>
2024-12-12 06:08:05 -0500

    WIP:

--- src/chains/Mantle/Common.sol
+++ src/chains/Mantle/Common.sol
@@ -1,29 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.25;
 
-import {SettlerBase} from "../SettlerBase.sol";
-import {Settler} from "../Settler.sol";
-import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
+import {SettlerBase} from "../../SettlerBase.sol";
 
 import {IERC20} from "@forge-std/interfaces/IERC20.sol";
-import {DodoV1, IDodoV1} from "../core/DodoV1.sol";
-import {DodoV2, IDodoV2} from "../core/DodoV2.sol";
-import {FreeMemory} from "../utils/FreeMemory.sol";
+import {DodoV1, IDodoV1} from "../../core/DodoV1.sol";
+import {DodoV2, IDodoV2} from "../../core/DodoV2.sol";
+import {FreeMemory} from "../../utils/FreeMemory.sol";
 
-import {ISettlerActions} from "../ISettlerActions.sol";
-import {UnknownForkId} from "../core/SettlerErrors.sol";
+import {ISettlerActions} from "../../ISettlerActions.sol";
+import {UnknownForkId} from "../../core/SettlerErrors.sol";
 
 import {
     uniswapV3MantleFactory,
     uniswapV3InitHash,
     uniswapV3ForkId,
     IUniswapV3Callback
-} from "../core/univ3forks/UniswapV3.sol";
+} from "../../core/univ3forks/UniswapV3.sol";
 
 // Solidity inheritance is stupid
-import {SettlerAbstract} from "../SettlerAbstract.sol";
-import {AbstractContext} from "../Context.sol";
-import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
+import {SettlerAbstract} from "../../SettlerAbstract.sol";
 
 abstract contract MantleMixin is FreeMemory, SettlerBase, DodoV1, DodoV2 {
     constructor() {
@@ -68,63 +64,5 @@ abstract contract MantleMixin is FreeMemory, SettlerBase, DodoV1, DodoV2 {
         } else {
             revert UnknownForkId(forkId);
         }
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract MantleSettler is Settler, MantleMixin {
-    constructor(bytes20 gitCommit) Settler(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data) internal override DANGEROUS_freeMemory returns (bool) {
-        return super._dispatchVIP(action, data);
-    }
-
-    // Solidity inheritance is stupid
-    function _isRestrictedTarget(address target)
-        internal
-        pure
-        override(Settler, Permit2PaymentAbstract)
-        returns (bool)
-    {
-        return super._isRestrictedTarget(target);
-    }
-
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, MantleMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(Settler, AbstractContext) returns (address) {
-        return super._msgSender();
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract MantleSettlerMetaTxn is SettlerMetaTxn, MantleMixin {
-    constructor(bytes20 gitCommit) SettlerMetaTxn(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data, bytes calldata sig)
-        internal
-        override
-        DANGEROUS_freeMemory
-        returns (bool)
-    {
-        return super._dispatchVIP(action, data, sig);
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, MantleMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(SettlerMetaTxn, AbstractContext) returns (address) {
-        return super._msgSender();
     }
 }
