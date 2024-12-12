4cc4052c5525599e57cedb5648337aa0979e4c2d
Duncan Townsend <git@duncancmt.com>
2024-12-12 06:19:51 -0500

    WIP:

--- src/chains/Taiko/Common.sol
+++ src/chains/Taiko/Common.sol
@@ -1,32 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.25;
 
-import {SettlerBase} from "../SettlerBase.sol";
-import {Settler} from "../Settler.sol";
-import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
+import {SettlerBase} from "../../SettlerBase.sol";
 
 import {IERC20} from "@forge-std/interfaces/IERC20.sol";
-import {FreeMemory} from "../utils/FreeMemory.sol";
+import {FreeMemory} from "../../utils/FreeMemory.sol";
 
-import {ISettlerActions} from "../ISettlerActions.sol";
+import {ISettlerActions} from "../../ISettlerActions.sol";
 import {ISignatureTransfer} from "@permit2/interfaces/ISignatureTransfer.sol";
-import {UnknownForkId} from "../core/SettlerErrors.sol";
+import {UnknownForkId} from "../../core/SettlerErrors.sol";
 
 import {
     uniswapV3TaikoFactory,
     uniswapV3InitHash,
     uniswapV3ForkId,
     IUniswapV3Callback
-} from "../core/univ3forks/UniswapV3.sol";
-import {swapsicleFactory, swapsicleInitHash, swapsicleForkId} from "../core/univ3forks/Swapsicle.sol";
-import {IAlgebraCallback} from "../core/univ3forks/Algebra.sol";
-import {pankoFactory, pankoInitHash, pankoForkId} from "../core/univ3forks/Panko.sol";
-import {IPancakeSwapV3Callback} from "../core/univ3forks/PancakeSwapV3.sol";
-
-// Solidity inheritance is stupid
-import {SettlerAbstract} from "../SettlerAbstract.sol";
-import {AbstractContext} from "../Context.sol";
-import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
+} from "../../core/univ3forks/UniswapV3.sol";
+import {swapsicleFactory, swapsicleInitHash, swapsicleForkId} from "../../core/univ3forks/Swapsicle.sol";
+import {IAlgebraCallback} from "../../core/univ3forks/Algebra.sol";
+import {pankoFactory, pankoInitHash, pankoForkId} from "../../core/univ3forks/Panko.sol";
+import {IPancakeSwapV3Callback} from "../../core/univ3forks/PancakeSwapV3.sol";
 
 abstract contract TaikoMixin is FreeMemory, SettlerBase {
     constructor() {
@@ -69,73 +62,5 @@ abstract contract TaikoMixin is FreeMemory, SettlerBase {
         } else {
             revert UnknownForkId(forkId);
         }
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract TaikoSettler is Settler, TaikoMixin {
-    constructor(bytes20 gitCommit) Settler(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data) internal override DANGEROUS_freeMemory returns (bool) {
-        if (super._dispatchVIP(action, data)) {
-            return true;
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    // Solidity inheritance is stupid
-    function _isRestrictedTarget(address target)
-        internal
-        pure
-        override(Settler, Permit2PaymentAbstract)
-        returns (bool)
-    {
-        return super._isRestrictedTarget(target);
-    }
-
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, TaikoMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(Settler, AbstractContext) returns (address) {
-        return super._msgSender();
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract TaikoSettlerMetaTxn is SettlerMetaTxn, TaikoMixin {
-    constructor(bytes20 gitCommit) SettlerMetaTxn(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data, bytes calldata sig)
-        internal
-        override
-        DANGEROUS_freeMemory
-        returns (bool)
-    {
-        if (super._dispatchVIP(action, data, sig)) {
-            return true;
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, TaikoMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(SettlerMetaTxn, AbstractContext) returns (address) {
-        return super._msgSender();
     }
 }
