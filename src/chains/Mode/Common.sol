221fc624de1f2ef07ea717a500a9f98d0eab3d99
Duncan Townsend <git@duncancmt.com>
2024-12-12 06:11:17 -0500

    WIP:

--- src/chains/Mode/Common.sol
+++ src/chains/Mode/Common.sol
@@ -1,30 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {SettlerBase} from "../SettlerBase.sol";
-import {Settler} from "../Settler.sol";
-import {SettlerMetaTxn} from "../SettlerMetaTxn.sol";
+import {SettlerBase} from "../../SettlerBase.sol";
 
-import {FreeMemory} from "../utils/FreeMemory.sol";
+import {FreeMemory} from "../../utils/FreeMemory.sol";
 
-import {ISettlerActions} from "../ISettlerActions.sol";
-import {UnknownForkId} from "../core/SettlerErrors.sol";
+import {ISettlerActions} from "../../ISettlerActions.sol";
+import {UnknownForkId} from "../../core/SettlerErrors.sol";
 
 import {
     supSwapV3Factory, supSwapV3InitHash, supSwapV3ForkId, ISupSwapV3Callback
-} from "../core/univ3forks/SupSwapV3.sol";
-import {kimFactory, kimInitHash, kimForkId} from "../core/univ3forks/Kim.sol";
-import {IAlgebraCallback} from "../core/univ3forks/Algebra.sol";
-import {swapModeV3Factory, swapModeV3InitHash, swapModeV3ForkId} from "../core/univ3forks/SwapModeV3.sol";
-import {IUniswapV3Callback} from "../core/univ3forks/UniswapV3.sol";
+} from "../../core/univ3forks/SupSwapV3.sol";
+import {kimFactory, kimInitHash, kimForkId} from "../../core/univ3forks/Kim.sol";
+import {IAlgebraCallback} from "../../core/univ3forks/Algebra.sol";
+import {swapModeV3Factory, swapModeV3InitHash, swapModeV3ForkId} from "../../core/univ3forks/SwapModeV3.sol";
+import {IUniswapV3Callback} from "../../core/univ3forks/UniswapV3.sol";
 
 import {MODE_SFS} from "./IModeSFS.sol";
 
 // Solidity inheritance is stupid
-import {SettlerAbstract} from "../SettlerAbstract.sol";
-import {AbstractContext} from "../Context.sol";
-import {Permit2PaymentAbstract} from "../core/Permit2PaymentAbstract.sol";
-import {Permit2PaymentBase} from "../core/Permit2Payment.sol";
+import {Permit2PaymentAbstract} from "../../core/Permit2PaymentAbstract.sol";
 
 abstract contract ModeMixin is FreeMemory, SettlerBase {
     constructor() {
@@ -73,67 +68,5 @@ abstract contract ModeMixin is FreeMemory, SettlerBase {
         } else {
             revert UnknownForkId(forkId);
         }
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract ModeSettler is Settler, ModeMixin {
-    constructor(bytes20 gitCommit) Settler(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data) internal override DANGEROUS_freeMemory returns (bool) {
-        return super._dispatchVIP(action, data);
-    }
-
-    function _isRestrictedTarget(address target) internal pure override(Settler, ModeMixin) returns (bool) {
-        return ModeMixin._isRestrictedTarget(target) || Settler._isRestrictedTarget(target);
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, ModeMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(Settler, AbstractContext) returns (address) {
-        return super._msgSender();
-    }
-}
-
-/// @custom:security-contact security@0x.org
-contract ModeSettlerMetaTxn is SettlerMetaTxn, ModeMixin {
-    constructor(bytes20 gitCommit) SettlerMetaTxn(gitCommit) {}
-
-    function _dispatchVIP(uint256 action, bytes calldata data, bytes calldata sig)
-        internal
-        override
-        DANGEROUS_freeMemory
-        returns (bool)
-    {
-        return super._dispatchVIP(action, data, sig);
-    }
-
-    function _isRestrictedTarget(address target)
-        internal
-        pure
-        override(Permit2PaymentBase, ModeMixin, Permit2PaymentAbstract)
-        returns (bool)
-    {
-        return ModeMixin._isRestrictedTarget(target) || Permit2PaymentBase._isRestrictedTarget(target);
-    }
-
-    // Solidity inheritance is stupid
-    function _dispatch(uint256 i, uint256 action, bytes calldata data)
-        internal
-        override(SettlerAbstract, SettlerBase, ModeMixin)
-        returns (bool)
-    {
-        return super._dispatch(i, action, data);
-    }
-
-    function _msgSender() internal view override(SettlerMetaTxn, AbstractContext) returns (address) {
-        return super._msgSender();
     }
 }
