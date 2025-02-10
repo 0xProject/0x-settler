526f6135120653f321ef4ebce14e62617f5d7896
Duncan Townsend <git@duncancmt.com>
2025-02-10 13:30:54 -0500

    Add `MultiCallContext` to read the forwarded sender

---
+++ src/multicall/MultiCallContext.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Context} from "../Context.sol";
+
+interface IMultiCall {
+    enum RevertPolicy {
+        REVERT,
+        STOP,
+        CONTINUE
+    }
+
+    struct Call {
+        address target;
+        RevertPolicy revertPolicy;
+        bytes data;
+    }
+
+    struct Result {
+        bool success;
+        bytes data;
+    }
+
+    function multicall(Call[] calldata, uint256 contextdepth) external returns (Result[] memory);
+}
+
+abstract contract MultiCallContext is Context {
+    IMultiCall internal constant _MULTICALL = IMultiCall(0x000000000000deaDdeAddEADdEaddeaDDEADDeAd); // TODO:
+
+    function _isForwarded() internal view virtual override returns (bool) {
+        return super._isForwarded() || super._msgSender() == address(_MULTICALL);
+    }
+
+    function _msgSender() internal view virtual override returns (address sender) {
+        sender = super._msgSender();
+        if (sender == address(_MULTICALL)) {
+            // ERC-2771. The trusted forwarder (`_MULTICALL`) has appended the appropriate
+            // msg.sender to the msg data
+            assembly ("memory-safe") {
+                sender := shr(0x60, calldataload(sub(calldatasize(), 0x14)))
+            }
+        }
+    }
+}
