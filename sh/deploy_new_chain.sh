1dd7cc38c1c35317df144dc0b469aede85986a29
Duncan Townsend <git@duncancmt.com>
2025-02-10 13:05:30 -0500

    Check that the balance of both deployers is nonzero when in `BROADCAST=yes` mode in `sh/deploy_new_chain.sh`

--- sh/deploy_new_chain.sh
+++ sh/deploy_new_chain.sh
@@ -251,6 +251,18 @@ declare -a maybe_broadcast=()
 if [[ ${BROADCAST-no} = [Yy]es ]] ; then
     maybe_broadcast+=(--broadcast)
 fi
+declare -r -a maybe_broadcast
+
+if [[ ${BROADCAST-no} = [Yy]es ]] ; then
+    if $(( $(cast balance --rpc-url "$rpc_url" "$module_deployer") == 0 )) ; then
+        echo 'You forgot to send ETH to '"$module_deployer"'.' >&2
+        exit 1
+    fi
+    if $(( $(cast balance --rpc-url "$rpc_url" "$proxy_deployer") == 0 )) ; then
+        echo 'You forgot to send ETH to '"$proxy_deployer"'.' >&2
+        exit 1
+    fi
+fi
 
 export FOUNDRY_OPTIMIZER_RUNS=1000000
 
