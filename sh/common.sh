d2429bfebbe9a2d0b18041a1bebdaf7cc780669e
Duncan Townsend <git@duncancmt.com>
2025-02-20 11:10:16 -0500

    Configure verification for the public MonadTestnet explorer

--- sh/common.sh
+++ sh/common.sh
@@ -102,18 +102,22 @@ function verify_contract {
     declare -r _verify_source_path="$1"
     shift
 
-    if (( chainid == 10143 )) || (( chainid == 34443 )) || (( chainid == 57073 )) ; then # MonadTestnet, Mode, and Ink use Blockscout, not Etherscan
+    if (( chainid == 34443 )) || (( chainid == 57073 )) ; then # Mode and Ink use Blockscout, not Etherscan
         declare _verify_blockscoutApi
-        if (( chainid == 10143 )) ; then # MonadTestnet is private. The explorer credentials are stored in the secrets file
-            _verify_blockscoutApi="$(get_api_secret blockscoutApi)"
-        else
-            _verify_blockscoutApi="$(get_config blockscoutApi)"
-        fi
+        _verify_blockscoutApi="$(get_config blockscoutApi)"
+        declare -r _verify_blockscoutApi
         forge verify-contract --watch --chain $chainid --verifier blockscout --verifier-url "$_verify_blockscoutApi" --constructor-args "$_verify_constructor_args" "$_verify_deployed_address" "$_verify_source_path"
-    else
+    elif (( chainid != 10143 )) ; then # MonadTestnet uses BlockVision (which works like Sourcify)
         forge verify-contract --watch --verifier custom --verifier-api-key "$(get_api_secret etherscanKey)" --verifier-url "$(get_config etherscanApi)" --constructor-args "$_verify_constructor_args" "$_verify_deployed_address" "$_verify_source_path"
     fi
-    if (( chainid != 146 )) && (( chainid != 480 )) && (( chainid != 10143 )) && (( chainid != 57073 )) && (( chainid != 81457 )) && (( chainid != 167000 )); then # Sourcify doesn't support Sonic, World Chain, MonadTestnet, Ink, Blast, or Taiko
-        forge verify-contract --watch --chain $chainid --verifier sourcify --constructor-args "$_verify_constructor_args" "$_verify_deployed_address" "$_verify_source_path"
+    if (( chainid != 146 )) && (( chainid != 480 )) && (( chainid != 57073 )) && (( chainid != 81457 )) && (( chainid != 167000 )); then # Sourcify doesn't support Sonic, World Chain, Ink, Blast, or Taiko
+        declare _verify_sourcifyApi
+        _verify_sourcifyApi="$(get_config sourcifyApi)"
+        declare -r _verify_sourcifyApi
+        if [[ ${_verify_sourcifyApi:-null} == null ]] ; then
+            forge verify-contract --watch --chain $chainid --verifier sourcify --constructor-args "$_verify_constructor_args" "$_verify_deployed_address" "$_verify_source_path"
+        else
+            forge verify-contract --watch --chain $chainid --verifier sourcify --verifier-url "$_verify_sourcifyApi" --constructor-args "$_verify_constructor_args" "$_verify_deployed_address" "$_verify_source_path"
+        fi
     fi
 }
