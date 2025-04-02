c7d7fb89451e1678a3cffee2069c63fa5b1e170d
Duncan Townsend <git@duncancmt.com>
2025-04-02 12:27:50 -0400

    Remove `VELODROME` action on Mainnet

--- src/chains/Mainnet/Common.sol
+++ src/chains/Mainnet/Common.sol
@@ -12,6 +12,8 @@ import {DodoV2, IDodoV2} from "../../core/DodoV2.sol";
 import {UniswapV4} from "../../core/UniswapV4.sol";
 import {IPoolManager} from "../../core/UniswapV4Types.sol";
 import {BalancerV3} from "../../core/BalancerV3.sol";
+
+import {SafeTransferLib} from "../../vendor/SafeTransferLib.sol";
 import {FreeMemory} from "../../utils/FreeMemory.sol";
 
 import {ISettlerActions} from "../../ISettlerActions.sol";
@@ -57,6 +59,9 @@ abstract contract MainnetMixin is
     UniswapV4,
     BalancerV3
 {
+    using SafeTransferLib for IERC20;
+    using SafeTransferLib for address payable;
+
     constructor() {
         assert(block.chainid == 1 || block.chainid == 31337);
     }
@@ -68,8 +73,53 @@ abstract contract MainnetMixin is
         DANGEROUS_freeMemory
         returns (bool)
     {
-        if (super._dispatch(i, action, data)) {
-            return true;
+        //// NOTICE: we re-implement the base `_dispatch` implementation here so that we can remove
+        //// the `VELODROME` action JUST on this chain because it does little-to-no volume.
+
+        if (action == uint32(ISettlerActions.RFQ.selector)) {
+            (
+                address recipient,
+                ISignatureTransfer.PermitTransferFrom memory permit,
+                address maker,
+                bytes memory makerSig,
+                IERC20 takerToken,
+                uint256 maxTakerAmount
+            ) = abi.decode(data, (address, ISignatureTransfer.PermitTransferFrom, address, bytes, IERC20, uint256));
+
+            fillRfqOrderSelfFunded(recipient, permit, maker, makerSig, takerToken, maxTakerAmount);
+        } else if (action == uint32(ISettlerActions.UNISWAPV3.selector)) {
+            (address recipient, uint256 bps, bytes memory path, uint256 amountOutMin) =
+                abi.decode(data, (address, uint256, bytes, uint256));
+
+            sellToUniswapV3(recipient, bps, path, amountOutMin);
+        } else if (action == uint32(ISettlerActions.UNISWAPV2.selector)) {
+            (address recipient, address sellToken, uint256 bps, address pool, uint24 swapInfo, uint256 amountOutMin) =
+                abi.decode(data, (address, address, uint256, address, uint24, uint256));
+
+            sellToUniswapV2(recipient, sellToken, bps, pool, swapInfo, amountOutMin);
+        } else if (action == uint32(ISettlerActions.BASIC.selector)) {
+            (IERC20 sellToken, uint256 bps, address pool, uint256 offset, bytes memory _data) =
+                abi.decode(data, (IERC20, uint256, address, uint256, bytes));
+
+            basicSellToPool(sellToken, bps, pool, offset, _data);
+        } /* `VELODROME` is removed */
+        else if (action == uint32(ISettlerActions.POSITIVE_SLIPPAGE.selector)) {
+            (address recipient, IERC20 token, uint256 expectedAmount) = abi.decode(data, (address, IERC20, uint256));
+            if (token == ETH_ADDRESS) {
+                uint256 balance = address(this).balance;
+                if (balance > expectedAmount) {
+                    unchecked {
+                        payable(recipient).safeTransferETH(balance - expectedAmount);
+                    }
+                }
+            } else {
+                uint256 balance = token.fastBalanceOf(address(this));
+                if (balance > expectedAmount) {
+                    unchecked {
+                        token.safeTransfer(recipient, balance - expectedAmount);
+                    }
+                }
+            }
         } else if (action == uint32(ISettlerActions.UNISWAPV4.selector)) {
             (
                 address recipient,
