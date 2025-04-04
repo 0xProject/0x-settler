b5ab860c6db0a2c0b3e8b9becdafeff133d0419d
Duncan Townsend <git@duncancmt.com>
2025-04-04 17:54:17 -0400

    Consistency

--- src/core/PancakeInfinity.sol
+++ src/core/PancakeInfinity.sol
@@ -25,13 +25,13 @@ library BalanceDeltaLibrary {
 
     function amount0(BalanceDelta balanceDelta) internal pure returns (int128 _amount0) {
         assembly ("memory-safe") {
-            _amount0 := sar(128, balanceDelta)
+            _amount0 := sar(0x80, balanceDelta)
         }
     }
 
     function amount1(BalanceDelta balanceDelta) internal pure returns (int128 _amount1) {
         assembly ("memory-safe") {
-            _amount1 := signextend(15, balanceDelta)
+            _amount1 := signextend(0x0f, balanceDelta)
         }
     }
 }
