e41889df2c26f27cc8783e1504bab0dd662d1d70
Lázaro Raúl Iglesias Vera <riglesiasvera@gmail.com>
2025-04-23 19:32:12 +0200

    fix: Foundry up installation suggested command

--- sh/common.sh
+++ sh/common.sh
@@ -11,7 +11,7 @@ forge_version="${forge_version:13:7}"
 declare -r forge_version
 if [[ $forge_version != '59f354c' ]] ; then
     echo 'Wrong foundry version installed -- '"$forge_version" >&2
-    echo 'Run `foundryup -v nightly-59f354c179f4e7f6d7292acb3d068815c79286d1`' >&2
+    echo 'Run `foundryup -i nightly-59f354c179f4e7f6d7292acb3d068815c79286d1`' >&2
     exit 1
 fi
 
