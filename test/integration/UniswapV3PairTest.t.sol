ce52d050da7a60ab095e8f02d6214bf434815a65
Duncan Townsend <git@duncancmt.com>
2025-04-23 16:55:42 -0400

    Unwrap WETH

--- test/integration/UniswapV3PairTest.t.sol
+++ test/integration/UniswapV3PairTest.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.25;
 
 import {IAllowanceTransfer} from "@permit2/interfaces/IAllowanceTransfer.sol";
 
-import {UNIVERSAL_ROUTER, encodePermit2Permit, encodeV3Swap} from "src/vendor/IUniswapUniversalRouter.sol";
+import {UNIVERSAL_ROUTER, encodePermit2Permit, encodeV3Swap, encodeUnwrapWeth} from "src/vendor/IUniswapUniversalRouter.sol";
 
 import {BasePairTest} from "./BasePairTest.t.sol";
 import {IUniswapV3Router} from "./vendor/IUniswapV3Router.sol";
@@ -38,15 +38,16 @@ abstract contract UniswapV3PairTest is BasePairTest {
     }
 
     function testUniswapV3UniversalRouter() public {
-        bytes memory commands = new bytes(2);
-        bytes[] memory inputs = new bytes[](2);
+        bytes memory commands = new bytes(3);
+        bytes[] memory inputs = new bytes[](3);
 
         IAllowanceTransfer.PermitSingle memory permit = defaultERC20PermitSingle(address(fromToken()), PERMIT2_FROM_NONCE);
         bytes memory signature = getPermitSingleSignature(permit, address(UNIVERSAL_ROUTER), FROM_PRIVATE_KEY, permit2Domain);
         bytes memory path = uniswapV3PathCompat();
 
         (commands[0], inputs[0]) = encodePermit2Permit(fromToken(), PERMIT2_FROM_NONCE, signature);
-        (commands[1], inputs[1]) = encodeV3Swap(FROM, amount(), 0, path, true);
+        (commands[1], inputs[1]) = encodeV3Swap(address(UNIVERSAL_ROUTER), amount(), 0 wei, path, true);
+        (commands[2], inputs[2]) = encodeUnwrapWeth(FROM, 0 wei);
 
         vm.startPrank(FROM, FROM);
         snapStartName("universalRouter_uniswapV3");
