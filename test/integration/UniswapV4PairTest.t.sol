eb639c2e7eab2a786250dfd6a971bdd85cfc818b
Duncan Townsend <git@duncancmt.com>
2025-04-23 17:05:11 -0400

    Fixing dumb mistakes

--- test/integration/UniswapV4PairTest.t.sol
+++ test/integration/UniswapV4PairTest.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import {IERC20} from "@forge-std/interfaces/IERC20.sol";
 import {IAllowanceTransfer} from "@permit2/interfaces/IAllowanceTransfer.sol";
 
 import {UNIVERSAL_ROUTER, encodePermit2Permit, encodeV4Swap} from "src/vendor/IUniswapUniversalRouter.sol";
@@ -9,14 +10,22 @@ import {BasePairTest} from "./BasePairTest.t.sol";
 
 abstract contract UniswapV4PairTest is BasePairTest {
     function testUniswapV4UniversalRouter() public {
-        bytes memory commands = new bytes(3);
-        bytes[] memory inputs = new bytes[](3);
+        bytes memory commands = new bytes(2);
+        bytes[] memory inputs = new bytes[](2);
 
         IAllowanceTransfer.PermitSingle memory permit = defaultERC20PermitSingle(address(fromToken()), PERMIT2_FROM_NONCE);
         bytes memory signature = getPermitSingleSignature(permit, address(UNIVERSAL_ROUTER), FROM_PRIVATE_KEY, permit2Domain);
 
         (commands[0], inputs[0]) = encodePermit2Permit(fromToken(), PERMIT2_FROM_NONCE, signature);
-        (commands[1], inputs[1]) = encodeV4Swap(FROM, amount(), 0 wei, fromToken(), 500, 10, address(0), toToken());
+        IERC20 toTokenCompat = toToken();
+        if (toTokenCompat == IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)) {
+            toTokenCompat = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+        }
+        IERC20 fromTokenCompat = fromToken();
+        if (fromTokenCompat == IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)) {
+            fromTokenCompat = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+        }
+        (commands[1], inputs[1]) = encodeV4Swap(FROM, amount(), 0 wei, fromTokenCompat, 500, 10, address(0), toTokenCompat);
 
         vm.startPrank(FROM, FROM);
         snapStartName("universalRouter_uniswapV4");
