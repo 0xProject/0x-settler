4590ff5d4d49bc0573d4d0feacdea978463895c7
Duncan Townsend <git@duncancmt.com>
2025-05-05 11:31:55 -0400

    Rename variable for consistency, avoidance of confusion

--- src/BridgeFactory.sol
+++ src/BridgeFactory.sol
@@ -104,15 +104,15 @@ contract BridgeFactory is IERC1271, MultiCallContext, TwoStepOwnable {
 
     error DeploymentFailed();
 
-    function deploy(bytes32 salt, address owner, bool setOwner) external noDelegateCall returns (address proxy) {
+    function deploy(bytes32 root, address owner, bool setOwner) external noDelegateCall returns (address proxy) {
         assembly ("memory-safe") {
             let ptr := mload(0x40)
 
             // derive the deployment salt from the owner and chainid
             mstore(0x34, chainid())
             mstore(0x14, owner)
-            mstore(0x00, salt)
-            salt := keccak256(0x00, 0x54)
+            mstore(0x00, root)
+            let salt := keccak256(0x00, 0x54)
 
             // create a minimal proxy targeting this contract
             mstore(0x1d, 0x5af43d5f5f3e6022573d5ffd5b3d5ff3)
