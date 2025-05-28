bfb2c090fc41ecef2961cfe8dbb6cb5433e24747
Duncan Townsend <git@duncancmt.com>
2025-05-28 16:12:01 -0400

    Pedantry

--- src/CrossChainReceiverFactory.sol
+++ src/CrossChainReceiverFactory.sol
@@ -14,6 +14,10 @@ import {Recover, PackedSignature} from "./utils/Recover.sol";
 interface IWrappedNative is IERC20 {
     function deposit() external payable;
     function withdraw(uint256) external;
+
+    event Deposit(address indexed, uint256);
+    event Withdrawal(address indexed, uint256);
+
     receive() external payable;
 }
 
