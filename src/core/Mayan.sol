2e5f46241211d8d27206b10d907473abdd2a3df7
Lázaro Raúl Iglesias Vera <riglesiasvera@gmail.com>
2025-06-17 23:18:09 +0200

    fix: masking

--- src/core/Mayan.sol
+++ src/core/Mayan.sol
@@ -16,7 +16,7 @@ contract Mayan {
             let size := mload(protocolData)
             mcopy(add(0x134, ptr), protocolData, add(0x20, size))
             mstore(add(0x114, ptr), 0x120)
-            mstore(add(0xf4, ptr), mayanProtocol)
+            mstore(add(0xf4, ptr), and(0xffffffffffffffffffffffffffffffffffffffff, mayanProtocol))
             // permit data is not going to be used as we are approving forwarder.
             // As it is not used, then we can send anything we have in memory
             // which is most likely empty but might be dirty. Even if it is dirty,
@@ -32,6 +32,9 @@ contract Mayan {
             // it is stored at 0x134, so we need to skip size (0x20), selector (0x04) and token (0x20)
             mstore(add(0x178, ptr), amount)
 
+            // `forwarder` is user provided and we don't check if it is a restricted target before calling it.
+            // It is fine to do so as this block only calls `forwardERC20` so there is no arbitrary
+            // execution and this selector doesn't collide with current restricted targets (AllowanceHolder & Permit2).
             if iszero(call(gas(), forwarder, 0x00, add(0x10, ptr), add(0x144, size), 0x00, 0x00)) {
                 let ptr_ := mload(0x40)
                 returndatacopy(ptr_, 0x00, returndatasize())
@@ -47,7 +50,7 @@ contract Mayan {
             let size := mload(protocolData)
             mcopy(add(0x54, ptr), protocolData, add(0x20, size))
             mstore(add(0x34, ptr), 0x40)
-            mstore(add(0x14, ptr), mayanProtocol)
+            mstore(add(0x14, ptr), and(0xffffffffffffffffffffffffffffffffffffffff, mayanProtocol))
             mstore(ptr, 0xb0f584ff000000000000000000000000) // selector for `forwardEth(address,bytes)` with `mayanProtocol` padding
 
             // modify copied amount in protocolData
@@ -55,6 +58,9 @@ contract Mayan {
             // it is stored at 0x54, so we need to skip size (0x20), selector (0x04) and token (0x20)
             mstore(add(0x98, ptr), selfbalance())
 
+            // `forwarder` is user provided and we don't check if it is a restricted target before calling it.
+            // It is fine to do so as this block only calls `forwardEth` so there is no arbitrary
+            // execution and this selector doesn't collide with current restricted targets (AllowanceHolder & Permit2).
             if iszero(call(gas(), forwarder, selfbalance(), add(0x10, ptr), add(0x64, size), 0x00, 0x00)) {
                 let ptr_ := mload(0x40)
                 returndatacopy(ptr_, 0x00, returndatasize())
