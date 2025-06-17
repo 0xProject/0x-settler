4e0cee5cbd14f572f154782f3bcc71a9aea9f100
Duncan Townsend <git@duncancmt.com>
2025-06-17 16:58:44 -0400

    Add missing overload

--- src/utils/Ternary.sol
+++ src/utils/Ternary.sol
@@ -16,6 +16,12 @@ library Ternary {
         }
     }
 
+    function ternary(bool c, address x, address y) internal pure returns (address r) {
+        assembly ("memory-safe") {
+            r := xor(y, mul(xor(x, y), c))
+        }
+    }
+
     function orZero(bool c, uint256 x) internal pure returns (uint256 r) {
         assembly ("memory-safe") {
             r := mul(x, c)
