a1d7816c36d4872e66a49c004131754c2262075b
Duncan Townsend <git@duncancmt.com>
2025-07-23 13:59:21 -0400

    Fixing CI

--- src/interfaces/IERC5267.sol
+++ src/interfaces/IERC5267.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.25;
 
 interface IERC5267 {
     /// @notice Returns the un-hashed fields that comprise the EIP-712 domain.
