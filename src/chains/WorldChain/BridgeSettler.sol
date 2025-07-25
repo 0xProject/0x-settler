c89b395b52376cd0a6901dd9df4c19fbeae2d4ab
Lázaro Raúl Iglesias Vera <riglesiasvera@gmail.com>
2025-07-25 13:33:10 +0200

    feat: World Chain Bridge Settler

--- src/chains/WorldChain/BridgeSettler.sol
+++ src/chains/WorldChain/BridgeSettler.sol
@@ -4,9 +4,29 @@ pragma solidity =0.8.25;
 import {SettlerAbstract} from "../../SettlerAbstract.sol";
 import {IBridgeSettlerActions} from "../../bridge/IBridgeSettlerActions.sol";
 import {BridgeSettler, BridgeSettlerBase} from "../../bridge/BridgeSettler.sol";
+import {Across} from "../../core/Across.sol";
 
-contract WorldChainBridgeSettler is BridgeSettler {
+contract WorldChainBridgeSettler is BridgeSettler, Across {
     constructor(bytes20 gitCommit) BridgeSettlerBase(gitCommit) {
         assert(block.chainid == 480 || block.chainid == 31337);
+    }
+
+    function _dispatch(uint256 i, uint256 action, bytes calldata data)
+        internal
+        override(BridgeSettlerBase, SettlerAbstract)
+        returns (bool)
+    {
+        if (super._dispatch(i, action, data)) {
+            return true;
+        } else if (action == uint32(IBridgeSettlerActions.BRIDGE_ERC20_TO_ACROSS.selector)) {
+            (address spoke, bytes memory depositData) = abi.decode(data, (address, bytes));
+            bridgeERC20ToAcross(spoke, depositData);
+        } else if (action == uint32(IBridgeSettlerActions.BRIDGE_NATIVE_TO_ACROSS.selector)) {
+            (address spoke, bytes memory depositData) = abi.decode(data, (address, bytes));
+            bridgeNativeToAcross(spoke, depositData);
+        } else {
+            return false;
+        }
+        return true;
     }
 }
