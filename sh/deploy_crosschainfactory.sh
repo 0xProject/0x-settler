e1d73d907841a6b3048236167d1dd67777a79b0d
Duncan Townsend <git@duncancmt.com>
2025-08-06 12:18:48 -0400

    Fixing `CrossChainReceiverFactory` deploy script

--- sh/deploy_crosschainfactory.sh
+++ sh/deploy_crosschainfactory.sh
@@ -126,11 +126,11 @@ if [[ $(cast keccak "$(cast code --rpc-url "$rpc_url" 0x4e59b44847b379578588920c
     exit 1
 fi
 
+. "$project_root"/sh/common_secrets.sh
+decrypt_secrets
 declare signer
-IFS='' read -p 'What address will you submit with?: ' -e -r -i 0xEf37aD2BACD70119F141140f7B5E46Cd53a65fc4 signer
+signer="$(get_secret wrappedNativeStorage deployer)"
 declare -r signer
-
-. "$project_root"/sh/common_wallet_type.sh
 
 # set minimum gas price to (mostly for Arbitrum and BNB)
 declare -i min_gas_price
@@ -147,41 +147,122 @@ declare -r -i gas_price
 
 export FOUNDRY_OPTIMIZER_RUNS=1000000
 export FOUNDRY_EVM_VERSION=london
+export FOUNDRY_SOLC_VERSION=0.8.28
 
 forge clean
 forge build src/CrossChainReceiverFactory.sol
 
 declare -r toehold=0x4e59b44847b379578588920cA78FbF26c0B4956C
+declare -r shim_initcode=0x326df01b1d1c8eef6c6cf71a0b658fbc1815601657fe5b7f60143603803560601c6df01b1d1c8eef6c6cf71a0b658fbc14336ccf9e3c5a263d527f621af382fa17f24f1416602e57fe5b3d54604b57583d55803d3d373d34f03d8159526d6045573dfd5b5260203df35b30ff60901b5952604e3df3
+declare -r crosschainfactory_salt=0x0000000000000000000000000000000000000009435af220071616d150499b5f
+
+declare shim_salt
+shim_salt="$(cast keccak 'Wrapped Native Token Address')"
+declare -r shim_salt
+
+declare crosschainfactory_initcode
+crosschainfactory_initcode="$(forge inspect src/CrossChainReceiverFactory.sol:CrossChainReceiverFactory bytecode)"
+declare -r crosschainfactory_initcode
+
+declare -r forwarding_multicall_sig='multicall((address,uint8,uint256,bytes)[],uint256)((bool,bytes)[])'
+declare -r non_forwarding_multicall_sig='multiSend(bytes)'
+
+declare shim
+shim="$(cast concat-hex 0xff "$toehold" "$shim_salt" "$(cast keccak "$shim_initcode")")"
+shim="$(cast keccak "$shim")"
+shim="${shim:26:40}"
+shim="$(cast to-check-sum-address "$shim")"
+declare -r shim
+
+declare wnative_storage
+wnative_storage="$(cast concat-hex 0xd694 "$shim" 0x01)"
+wnative_storage="$(cast keccak "$wnative_storage")"
+wnative_storage="${wnative_storage:26:40}"
+wnative_storage="$(cast to-check-sum-address "$wnative_storage")"
+declare -r wnative_storage
+
+declare non_forwarding_multicall
+non_forwarding_multicall="$(get_config safe.multiCall)"
+declare -r non_forwarding_multicall
+
+declare deploy_shim_calldata
+deploy_shim_calldata="$(cast concat-hex 0x00 "$toehold" "$(cast to-uint256 0)" "$(cast to-uint256 "$(((${#shim_initcode} - 2) / 2 + 32))")" "$(cast concat-hex "$shim_salt" "$shim_initcode")")"
+deploy_shim_calldata="$(cast calldata "$non_forwarding_multicall_sig" "$deploy_shim_calldata")"
+declare -r deploy_shim_calldata
+
+declare deploy_crosschainfactory_calldata
+deploy_crosschainfactory_calldata="$(cast concat-hex 0x00 "$toehold" "$(cast to-uint256 2)" "$(cast to-uint256 "$(((${#crosschainfactory_initcode} - 2) / 2 + 32))")" "$(cast concat-hex "$crosschainfactory_salt" "$crosschainfactory_initcode")")"
+deploy_crosschainfactory_calldata="$(cast calldata "$non_forwarding_multicall_sig" "$deploy_crosschainfactory_calldata")"
+declare -r deploy_crosschainfactory_calldata
+
+declare forwarding_multicall
+forwarding_multicall="$(get_config deployment.forwardingMultiCall)"
+declare -r forwarding_multicall
+
+declare wnative
+wnative="$(get_config wnative)"
+declare -r wnative
+
+declare wnative_storage_initcode
+wnative_storage_initcode="$(cast concat-hex 0x7f30ff00000000000000000000 "$wnative" 0x5f52595ff3)"
+declare -r wnative_storage_initcode
+
+declare -a deploy_calldatas=()
+deploy_calldatas+=(
+    # target, revert policy, value, data
+    "$non_forwarding_multicall" 0 0wei "$deploy_shim_calldata"
+    "$shim" 0 0wei "$wnative_storage_initcode"
+    "$shim" 0 0wei 0x00000000
+    "$wnative_storage" 0 0wei 0x00000000
+    "$non_forwarding_multicall" 0 2wei "$deploy_crosschainfactory_calldata"
+)
 
 declare deploy_calldata
-deploy_calldata="$(cast concat-hex 0x0000000000000000000000000000000000000009435af220071616d150499b5f "$(forge inspect src/CrossChainReceiverFactory.sol:CrossChainReceiverFactory bytecode)")"
+while (( ${#deploy_calldatas[@]} >= 4 )) ; do
+    declare target="${deploy_calldatas[0]}"
+    declare -i revert_policy="${deploy_calldatas[1]}"
+    declare value="${deploy_calldatas[2]}"
+    declare data="${deploy_calldatas[3]}"
+    deploy_calldatas=( "${deploy_calldatas[@]:4:$((${#deploy_calldatas[@]}-4))}" )
+
+    if [[ -z ${deploy_calldata-} ]] ; then
+        deploy_calldata='['
+    else
+        deploy_calldata="$deploy_calldata"','
+    fi
+    deploy_calldata="$deploy_calldata"'('"$target"','"$revert_policy"','"$value"','"$data"')'
+done
+deploy_calldata="$(cast calldata "$forwarding_multicall_sig" "$deploy_calldata"']' 0)"
 declare -r deploy_calldata
+
+set -x
 
 declare -i gas_estimate_multiplier
 gas_estimate_multiplier="$(get_config gasMultiplierPercent)"
 declare -r -i gas_estimate_multiplier
 declare -i gas_limit
-gas_limit="$(cast estimate --from "$signer" --rpc-url "$rpc_url" --gas-price $gas_price --chain $chainid "$toehold" "$deploy_calldata")"
+if [[ ${BROADCAST-no} = [Yy]es ]] ; then
+    gas_limit="$(cast estimate --from "$signer" --chain $chainid --value 2wei --rpc-url "$rpc_url" --gas-price $gas_price "$forwarding_multicall" "$deploy_calldata")"
+else
+    gas_limit=16777215
+fi
 gas_limit=$((gas_limit * gas_estimate_multiplier / 100))
+if (( gas_limit > 16777215 )) ; then
+    gas_limit=16777215
+fi
 declare -r -i gas_limit
 
 declare -a maybe_broadcast=()
-declare submit_rpc
 if [[ ${BROADCAST-no} = [Yy]es ]] ; then
-    maybe_broadcast+=(send --chain $chainid)
-    if [[ $wallet_type = 'frame' ]] ; then
-        submit_rpc='http://127.0.0.1:1248'
-        maybe_broadcast+=(--unlocked)
-    else
-        submit_rpc="$rpc_url"
-    fi
+    exit 1
+    maybe_broadcast+=(send --chain $chainid --private-key)
+    maybe_broadcast+=("$(get_secret wrappedNativeStorage key)")
 else
     maybe_broadcast+=(call --trace -vvvv)
-    submit_rpc="$rpc_url"
 fi
 declare -r -a maybe_broadcast
 
-cast "${maybe_broadcast[@]}" --from "$signer" --rpc-url "$submit_rpc" --gas-price $gas_price --gas-limit $gas_limit $(get_config extraFlags) "$toehold" "$deploy_calldata"
+cast "${maybe_broadcast[@]}" --from "$signer" --value 2wei --rpc-url "$rpc_url" --gas-price $gas_price --gas-limit $gas_limit $(get_config extraFlags) "$forwarding_multicall" "$deploy_calldata"
 
 if [[ ${BROADCAST-no} = [Yy]es ]] ; then
     sleep 60
