9a6cab7583f0dd8fe7824ca9276a3c8a61f9bac8
Duncan Townsend <git@duncancmt.com>
2025-08-14 18:07:05 -0400

    Golf

--- src/core/Basic.sol
+++ src/core/Basic.sol
@@ -8,22 +8,19 @@ import {IERC20} from "@forge-std/interfaces/IERC20.sol";
 import {SafeTransferLib} from "../vendor/SafeTransferLib.sol";
 import {FullMath} from "../vendor/FullMath.sol";
 import {Panic} from "../utils/Panic.sol";
-import {Revert} from "../utils/Revert.sol";
 import {UnsafeMath} from "../utils/UnsafeMath.sol";
 
 abstract contract Basic is SettlerAbstract {
     using UnsafeMath for uint256;
+    using SafeTransferLib for address payable;
     using SafeTransferLib for IERC20;
     using FullMath for uint256;
-    using Revert for bool;
 
     /// @dev Sell to a pool with a generic approval, transferFrom interaction.
     /// offset in the calldata is used to update the sellAmount given a proportion of the sellToken balance
     function basicSellToPool(IERC20 sellToken, uint256 bps, address pool, uint256 offset, bytes memory data) internal {
-        if (_isRestrictedTarget(pool)) {
-            revertConfusedDeputy();
-        }
         {
+            bool condition = _isRestrictedTarget(pool);
             // This check is NOT an exhaustive check. There are many tokens that have alternative
             // allowance-spending methods, including (also nonexhaustively) DAI's `pull`, ERC677 and
             // ERC1363's `transferFromAndCall`, ERC777's `operatorSend`, and LZ OFT's `sendFrom` and
@@ -32,11 +29,11 @@ abstract contract Basic is SettlerAbstract {
             // could result in loss of funds. Fundamentally, though, for correct operation, it is
             // forbidden to set allowances on this contract. The fact that this does not cover all
             // cases IS NOT A BUG.
-            uint256 selector;
             assembly ("memory-safe") {
-                selector := mul(lt(0x00, mload(data)), and(0xffffffff, mload(add(0x04, data))))
+                // `0x23b872dd` is the selector for `transferFrom(address,address,uint256)`
+                condition := or(iszero(shl(0xe0, xor(0x23b872dd, mload(add(0x04, data))))), condition)
             }
-            if (selector == uint32(IERC20.transferFrom.selector)) {
+            if (condition) {
                 revertConfusedDeputy();
             }
         }
@@ -46,12 +43,17 @@ abstract contract Basic is SettlerAbstract {
         uint256 value;
         if (sellToken == ETH_ADDRESS) {
             unchecked {
+                // `bps > BASIS` will result in a revert when we try to send more ETH than we have
                 value = (address(this).balance * bps).unsafeDiv(BASIS);
             }
             if (data.length == 0) {
-                if (offset != 0) revert InvalidOffset();
-                (success, returnData) = payable(pool).call{value: value}("");
-                success.maybeRevert(returnData);
+                if (offset != 0) {
+                    assembly ("memory-safe") {
+                        mstore(0x00, 0x01da1572) // selector for `InvalidOffset()`
+                        revert(0x1c, 0x04)
+                    }
+                }
+                payable(pool).safeTransferETH(value);
                 return;
             } else {
                 if ((offset += 32) > data.length) {
@@ -62,8 +64,13 @@ abstract contract Basic is SettlerAbstract {
                 }
             }
         } else if (address(sellToken) == address(0)) {
-            // TODO: check for zero `bps`
-            if (offset != 0) revert InvalidOffset();
+            // `bps != 0` is treated as a GIGO error
+            if (offset != 0) {
+                assembly ("memory-safe") {
+                    mstore(0x00, 0x01da1572) // selector for `InvalidOffset()`
+                    revert(0x1c, 0x04)
+                }
+            }
         } else {
             // We treat `bps > BASIS` as a GIGO error
             uint256 amount = sellToken.fastBalanceOf(address(this)).unsafeMulDiv(bps, BASIS);
@@ -78,9 +85,19 @@ abstract contract Basic is SettlerAbstract {
                 sellToken.safeApproveIfBelow(pool, amount);
             }
         }
-        (success, returnData) = payable(pool).call{value: value}(data);
-        success.maybeRevert(returnData);
-        // forbid sending data to EOAs
-        if (returnData.length == 0 && pool.code.length == 0) revert InvalidTarget();
+        assembly ("memory-safe") {
+            if iszero(call(gas(), pool, value, add(0x20, data), mload(data), 0x00, 0x00)) {
+                let ptr := mload(0x40)
+                returndatacopy(ptr, 0x00, returndatasize())
+                revert(ptr, returndatasize())
+            }
+            if iszero(returndatasize()) {
+                if iszero(extcodesize(pool)) {
+                    // forbid sending data to EOAs
+                    mstore(0x00, 0x82d5d76a) // selector for `InvalidTarget()`
+                    revert(0x1c, 0x04)
+                }
+            }
+        }
     }
 }
