95f94997808282cd5b6852d8cdb98de4a4a60649
Duncan Townsend <git@duncancmt.com>
2025-08-19 10:21:18 -0400

    Golf

--- test/0.8.25/SafeGuard.t.sol
+++ test/0.8.25/SafeGuard.t.sol
@@ -197,13 +197,10 @@ contract TestSafeGuard is Test {
 
         // Heck yeah, bubble sort
         {
-            Vm.Wallet memory tmp;
             for (uint256 i = 1; i < owners.length; i++) {
                 for (uint256 j = i; j > 0; j--) {
                     if (owners[j - 1].addr > owners[j].addr) {
-                        tmp = owners[j - 1];
-                        owners[j - 1] = owners[j];
-                        owners[j] = tmp;
+                        (owners[j - 1], owners[j]) =  (owners[j], owners[j - 1]);
                     }
                 }
             }
