d7ab88e228caf43bf76eba8a0b74cc33c84766e5
Lázaro Raúl Iglesias Vera <riglesiasvera@gmail.com>
2025-08-23 13:41:46 +0200

    test: DeBridge integration test

--- src/core/DeBridge.sol
+++ src/core/DeBridge.sol
@@ -4,7 +4,54 @@ pragma solidity ^0.8.25;
 import {IERC20} from "@forge-std/interfaces/IERC20.sol";
 import {SafeTransferLib} from "../vendor/SafeTransferLib.sol";
 
-address constant DLN_SOURCE = 0xeF4fB24aD0916217251F553c0596F8Edc630EB66;
+interface IDlnSource {
+    /// @dev Struct representing the creation parameters for creating an order on the (EVM) chain.
+    struct OrderCreation {
+        /// Address of the ERC-20 token that the maker is offering as part of this order.
+        /// Use the zero address to indicate that the maker is offering a native blockchain token (such as Ether, Matic, etc.).
+        address giveTokenAddress;
+        /// Amount of tokens the maker is offering.
+        uint256 giveAmount;
+        /// Address of the ERC-20 token that the maker is willing to accept on the destination chain.
+        bytes takeTokenAddress;
+        /// Amount of tokens the maker is willing to accept on the destination chain.
+        uint256 takeAmount;
+        // the ID of the chain where an order should be fulfilled.
+        uint256 takeChainId;
+        /// Address on the destination chain where funds should be sent upon order fulfillment.
+        bytes receiverDst;
+        /// Address on the source (current) chain authorized to patch the order by adding more input tokens, making it more attractive to takers.
+        address givePatchAuthoritySrc;
+        /// Address on the destination chain authorized to patch the order by reducing the take amount, making it more attractive to takers,
+        /// and can also cancel the order in the take chain.
+        bytes orderAuthorityAddressDst;
+        // An optional address restricting anyone in the open market from fulfilling
+        // this order but the given address. This can be useful if you are creating a order
+        // for a specific taker. By default, set to empty bytes array (0x)
+        bytes allowedTakerDst;
+        /// An optional external call data payload.
+        bytes externalCall;
+        // An optional address on the source (current) chain where the given input tokens
+        // would be transferred to in case order cancellation is initiated by the orderAuthorityAddressDst
+        // on the destination chain. This property can be safely set to an empty bytes array (0x):
+        // in this case, tokens would be transferred to the arbitrary address specified
+        // by the orderAuthorityAddressDst upon order cancellation
+        bytes allowedCancelBeneficiarySrc;
+    }
+
+    function createSaltedOrder(
+        OrderCreation calldata _orderCreation,
+        uint64 _salt,
+        bytes calldata _affiliateFee,
+        uint32 _referralCode,
+        bytes calldata _permitEnvelope,
+        bytes memory _metadata
+    ) external payable returns (bytes32);
+
+    function globalFixedNativeFee() external view returns (uint256);
+}
+
+IDlnSource constant DLN_SOURCE = IDlnSource(0xeF4fB24aD0916217251F553c0596F8Edc630EB66);
 
 contract DeBridge {
     using SafeTransferLib for IERC20;
@@ -18,30 +65,29 @@ contract DeBridge {
             value := selfbalance()
         }
         // Store the constant into source to read it only once
-        address source = DLN_SOURCE;
-        if(address(inputToken) == address(0)) {
+        IDlnSource source = DLN_SOURCE;
+        if (address(inputToken) == address(0)) {
             _bridgeNativeToDeBridge(source, value, value - globalFee, createOrderData);
-        }
-        else {
+        } else {
             uint256 amount = inputToken.fastBalanceOf(address(this));
-            inputToken.safeApproveIfBelow(source, amount);
-            
-            _bridgeNativeToDeBridge(source, value, value - globalFee, createOrderData);
+            inputToken.safeApproveIfBelow(address(source), amount);
+
+            _bridgeNativeToDeBridge(source, value, amount, createOrderData);
         }
     }
 
-    function _bridgeNativeToDeBridge(address source, uint256 value, uint256 amount, bytes memory createOrderData)
+    function _bridgeNativeToDeBridge(IDlnSource source, uint256 value, uint256 amount, bytes memory createOrderData)
         internal
     {
         assembly ("memory-safe") {
             // override giveAmount
-            mstore(0x100, amount)
+            mstore(add(0x100, createOrderData), amount)
 
             let len := mload(createOrderData)
             // temporarily clobber `createOrderData` size memory area
             mstore(createOrderData, 0xb9303701) // selector for `createSaltedOrder((address,uint256,bytes,uint256,uint256,bytes,address,bytes,bytes,bytes,bytes),uint64,bytes,uint32,bytes,bytes)`
             // `createSaltedOrder` doesn't clash with any relevant function of restricted targets so we can skip checking `source`
-            // `source` is also meant to be DLN_SOURCE 
+            // `source` is also meant to be DLN_SOURCE
             if iszero(call(gas(), source, value, add(0x1c, createOrderData), add(0x04, len), 0x00, 0x00)) {
                 let ptr := mload(0x40)
                 returndatacopy(ptr, 0x00, returndatasize())
