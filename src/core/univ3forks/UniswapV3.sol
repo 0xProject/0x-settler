dce0e79db57a3eddde032874527177aa2c29986c
Duncan Townsend <git@duncancmt.com>
2025-11-08 15:05:42 +0100

    Document EraVm UniswapV3 inithash

--- src/core/univ3forks/UniswapV3.sol
+++ src/core/univ3forks/UniswapV3.sol
@@ -22,6 +22,8 @@ address constant uniswapV3AbstractSepoliaFactory = 0x2E17FF9b877661bDFEF8879a4B3
 address constant uniswapV3AbstractFactory = 0xA1160e73B63F322ae88cC2d8E700833e71D0b2a1;
 
 bytes32 constant uniswapV3InitHash = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+// This isn't a "hash" inasmuch as it's a versioned discriminator
+// ref: https://web.archive.org/web/20251108134721/https://matter-labs.github.io/zksync-era/core/latest/guides/advanced/12_alternative_vm_intro.html#bytecode-hashes
 bytes32 constant uniswapV3InitHashEraVm = 0x010013f177ea1fcbc4520f9a3ca7cd2d1d77959e05aa66484027cb38e712aeed;
 uint8 constant uniswapV3ForkId = 0;
 
