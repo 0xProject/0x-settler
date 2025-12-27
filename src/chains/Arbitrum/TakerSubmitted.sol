ef859f8931aaee23b2bbc13f2659d37463379986
Duncan Townsend <git@duncancmt.com>
2025-12-27 08:38:30 +0100

    Add Bebop (`BEBOP` action) to Arbitrum

--- src/chains/Arbitrum/TakerSubmitted.sol
+++ src/chains/Arbitrum/TakerSubmitted.sol
@@ -11,7 +11,6 @@ import {ISettlerActions} from "../../ISettlerActions.sol";
 // Solidity inheritance is stupid
 import {SettlerAbstract} from "../../SettlerAbstract.sol";
 import {SettlerBase} from "../../SettlerBase.sol";
-import {Permit2PaymentAbstract} from "../../core/Permit2PaymentAbstract.sol";
 import {AbstractContext} from "../../Context.sol";
 
 /// @custom:security-contact security@0x.org
@@ -83,7 +82,7 @@ contract ArbitrumSettler is Settler, ArbitrumMixin {
     function _isRestrictedTarget(address target)
         internal
         view
-        override(Settler, Permit2PaymentAbstract)
+        override(Settler, ArbitrumMixin)
         returns (bool)
     {
         return super._isRestrictedTarget(target);
