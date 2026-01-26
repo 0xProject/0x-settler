838cdc365f97abc528c3cc76f58bd43d562acb6a
Lázaro Raúl Iglesias Vera <riglesiasvera@gmail.com>
2026-01-26 15:18:57 +0100

    feat: Check VIPs on CI

---
+++ js/vip-signature-checker.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+const artifactPath = './out/ISettlerActions.sol/ISettlerActions.json';
+const artifact = JSON.parse(fs.readFileSync(artifactPath, 'utf8'));
+const abi = artifact.abi;
+
+function validVip(item) {
+    const inputs = item.inputs || [];
+    if (inputs.length < 2) {
+        return false;
+    }
+    const recipient = inputs[0];
+    const permit = inputs[1];
+
+    // check recipient
+    if (recipient.type !== 'address' || recipient.name !== 'recipient') {
+        return false;
+    }
+
+    // check permit
+    return permit.type === 'tuple' && 
+        permit.internalType === 'struct ISignatureTransfer.PermitTransferFrom' && 
+        (permit.name === 'permit' || permit.name === 'takerPermit');
+}
+
+// Find all VIP functions
+const malformedVIPs = abi.filter(item =>
+    item.type === 'function' &&
+    item.name &&
+    item.name.endsWith('_VIP')
+).filter(item => !validVip(item));
+
+if (malformedVIPs.length > 0) {
+    console.error(
+        `Malformed VIP functions: ${malformedVIPs.map(item => item.name).join(', ')}`
+    );
+    process.exit(1);
+}
