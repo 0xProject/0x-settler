9d78bd9fa766b2c87137a677476b8939d14b7ed8
Duncan Townsend <git@duncancmt.com>
2026-02-09 09:54:54 +0100

    WIP: Tempo

--- src/deployer/SafeModule.sol
+++ src/deployer/SafeModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity =0.8.25;
 
 import {DEPLOYER} from "./DeployerAddress.sol";
 import {IDeployer, IDeployerRemove} from "./IDeployer.sol";
